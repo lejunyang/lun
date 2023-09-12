9b39a2f3174bee58f6de846e1634c2f319990bfa
lejunyang <lejunyang@qq.com>
2023-09-12 16:33:37 +0800

    chore: add vue-jsx plugin, define custom element rule, add an alias

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue({
+			template: {
+				compilerOptions: {
+					isCustomElement: (tag) => tag.startsWith('l-'),
+				},
+			},
+		}),
+		vueJsx(),
+	],
 	optimizeDeps: {
 		exclude: ['vue'],
 	},
@@ -12,19 +22,20 @@ export default defineConfig({
 		// __DEV__: "!!(process.env.NODE_ENV !== 'production')",
 		__DEV__: 'true',
 	},
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'custom': fileURLToPath(new URL('./src/custom', import.meta.url)),
-    }
-  },
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			custom: fileURLToPath(new URL('./src/custom', import.meta.url)),
+			utils: fileURLToPath(new URL('./src/utils', import.meta.url)),
+		},
+	},
 	css: {
 		// css预处理器
 		preprocessorOptions: {
 			scss: {
 				// 引入 mixin.scss 这样就可以在全局中使用 mixin.scss中预定义的变量了
 				// 给导入的路径最后加上 ;
-				additionalData: '@import "@/assets/style/mixin.scss";',
+				// additionalData: '@import "@/assets/style/mixin.scss";',
 			},
 		},
 	},
