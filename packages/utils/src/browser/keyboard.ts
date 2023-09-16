ad86540cbe03971227d39e7fcd17e5e7f2deb01f
lejunyang <lejunyang@qq.com>
2023-09-17 00:50:15 +0800

    feat(utils): `isKeyCompositionDown`

--- packages/utils/src/browser/keyboard.ts
+++ packages/utils/src/browser/keyboard.ts
@@ -1,3 +1,30 @@
-export function isEnterDownEvent(e: KeyboardEvent) {
-  return e.key === 'Enter';
+export function isEnterDown(e: KeyboardEvent) {
+	return e.key === 'Enter';
+}
+
+/**
+ * determine whether a key composition has been entered down
+ * @param e KeyDown Event
+ * @param keyPattern A string or string array representing a key composition pattern, for example `Ctrl + Alt + A`
+ * Modifiers and keys are case-insensitive. Both `ctrl` and `control` represent the control key, `shift` represent the shift key and `alt` represent the alt key.
+ * `meta`, `win`, `cmd`, `windows` and `command`, they all represent the meta key, which is Win in windows and Command in mac.
+ * @returns `undefined` if it's not keydown event, otherwise `boolean`
+ */
+export function isKeyCompositionDown(e: KeyboardEvent, keyPattern: string | string[]) {
+  if (e.type !== 'keydown') return;
+	const keySplits = Array.isArray(keyPattern) ? keyPattern : keyPattern.split('+');
+	let result = true;
+	for (let i = 0; i < keySplits.length; i++) {
+    const k = keySplits[i];
+    if (!k) continue;
+		const finalK = String(k).trim().toLowerCase();
+		if (!finalK) continue;
+		if (['ctrl', 'control'].includes(finalK)) result &&= e.ctrlKey;
+		else if (finalK === 'shift') result &&= e.shiftKey;
+		else if (finalK === 'alt') result &&= e.altKey;
+		else if (['meta', 'win', 'cmd', 'windows', 'command'].includes(finalK)) result &&= e.metaKey;
+		else result &&= finalK === e.key;
+		if (!result) break;
+	}
+	return result;
 }
