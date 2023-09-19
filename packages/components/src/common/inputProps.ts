7b597bfa55917592a1ab037d389e8dd8b7728fbc
lejunyang <lejunyang@qq.com>
2023-09-20 00:29:43 +0800

    feat(components/input): add input props

--- packages/components/src/common/inputProps.ts
+++ packages/components/src/common/inputProps.ts
@@ -1,7 +1,11 @@
-import { InputPeriod } from '@lun/core';
+import { InputPeriod, InputType } from '@lun/core';
 import { PropType } from 'vue';
 
 export const inputProps = Object.freeze({
+	value: { type: String },
+	placeholder: { type: String },
+	required: { type: String },
+	type: { type: String as PropType<InputType> },
 	changeWhen: { type: String as PropType<InputPeriod>, default: 'notComposing' },
 	wait: { type: Number },
 	waitType: { type: String as PropType<'throttle' | 'debounce'>, default: 'debounce' },
@@ -12,4 +16,16 @@ export const inputProps = Object.freeze({
 	toNullWhenEmpty: { type: Boolean, default: true },
 	transform: { type: Function as PropType<(value: string | null) => any> },
 	transformWhen: { type: String as PropType<InputPeriod>, default: 'notComposing' },
+	emitEnterDownWhenComposing: { type: Boolean },
 });
+
+export const inputNumberProps = Object.freeze({
+	...inputProps,
+	min: { type: Number },
+	max: { type: Number },
+	precision: { type: Number },
+	step: { type: Number },
+	strictStep: { type: Boolean },
+	noExponent: { type: Boolean },
+	optimizeChPeriodSymbolForNum: { type: Boolean, default: true }
+})
