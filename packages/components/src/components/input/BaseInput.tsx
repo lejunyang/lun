2ebbce710418c7fc396ffae67e231220d6d34e61
lejunyang <lejunyang@qq.com>
2023-09-20 00:32:57 +0800

    feat(components/input): add `BaseInput` components

---
+++ packages/components/src/components/input/BaseInput.tsx
@@ -0,0 +1,69 @@
+import { useInput, useSetupEdit } from '@lun/core';
+import { defineCustomFormElement } from 'custom';
+import { setDefaultsForPropOptions } from 'utils';
+import { editStateProps, inputProps } from 'common';
+import { GlobalStaticConfig } from 'config';
+import { useSetupContextEvent, useVModelCompatible } from 'hooks';
+
+const LBaseInput = defineCustomFormElement({
+	name: GlobalStaticConfig.nameMap['base-input'],
+	inheritAttrs: false,
+	props: {
+		...editStateProps,
+		...setDefaultsForPropOptions(inputProps, GlobalStaticConfig.defaultProps['base-input']),
+	},
+	styles: GlobalStaticConfig.styles['base-input'],
+	emits: ['update', 'enterDown'],
+	setup(props, { emit, attrs }) {
+		useSetupContextEvent();
+		const [editComputed] = useSetupEdit();
+		const { updateVModel } = useVModelCompatible();
+
+		const [inputHandlers] = useInput(() => ({
+			...attrs,
+			...props,
+			onChange: (val) => {
+				updateVModel(val);
+				emit('update', val);
+			},
+			onEnterDown(e) {
+				emit('enterDown', e);
+			},
+		}));
+
+		return () => (
+			<input
+				{...attrs}
+				type={props.type === 'number-text' ? 'tel' : props.type}
+				part="input"
+				class={['l-base-input']}
+				value={props.value}
+				placeholder={props.placeholder}
+				disabled={editComputed.value.disabled}
+				readonly={editComputed.value.readonly}
+				{...inputHandlers}
+			/>
+		);
+	},
+});
+
+export function defineBaseInput(name?: string) {
+	name ||= GlobalStaticConfig.nameMap['base-input'];
+	if (!customElements.get(name)) {
+		customElements.define(name, LBaseInput);
+	}
+}
+
+export default LBaseInput;
+
+declare module 'vue' {
+	export interface GlobalComponents {
+		LBaseInput: typeof LBaseInput;
+	}
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'l-base-input': typeof LBaseInput;
+	}
+}
