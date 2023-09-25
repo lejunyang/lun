69b6ec8569c265ab12c0099baaa423d3a649ec88
lejunyang <lejunyang@qq.com>
2023-09-26 00:41:41 +0800

    fix(utils/dom): fix `getPreviousMatchElInTree` when shadow dom or parent sibling element with children, add `getPreviousMatchNodeInTree`

--- packages/utils/src/browser/dom.ts
+++ packages/utils/src/browser/dom.ts
@@ -11,10 +11,41 @@ export function getPreviousMatchElInTree(
       if (isMatch(temp.previousElementSibling)) return temp.previousElementSibling;
       else temp = temp.previousElementSibling;
     } else {
-      const parent = temp.parentElement;
+      const parent = temp.parentElement || (temp.parentNode as ShadowRoot)?.host;
       if (!parent || needStop(parent)) return null;
       else if (isMatch(parent)) return parent;
-      else temp = parent;
+      else {
+        const siblingLastChild = parent.previousElementSibling?.lastElementChild;
+        if (!siblingLastChild) temp = parent;
+        else if (isMatch(siblingLastChild)) return siblingLastChild;
+        else temp = siblingLastChild;
+      }
+    }
+  }
+}
+
+export function getPreviousMatchNodeInTree(
+  node?: Node | null,
+  options?: { isMatch?: (node: Node) => boolean; needStop?: (node: Node) => boolean }
+) {
+  if (!node) return null;
+  let isMatch = options?.isMatch || ((node) => !!node);
+  let needStop = options?.needStop || ((node) => node.parentNode === document.documentElement);
+  let temp = node;
+  while (true) {
+    if (temp.previousSibling) {
+      if (isMatch(temp.previousSibling)) return temp.previousSibling;
+      else temp = temp.previousSibling;
+    } else {
+      const parent = temp.parentNode || (temp as ShadowRoot).host;
+      if (!parent || needStop(parent)) return null;
+      else if (isMatch(parent)) return parent;
+      else {
+        const siblingLastChild = parent.previousSibling?.lastChild;
+        if (!siblingLastChild) temp = parent;
+        else if (isMatch(siblingLastChild)) return siblingLastChild;
+        else temp = siblingLastChild;
+      }
     }
   }
 }
