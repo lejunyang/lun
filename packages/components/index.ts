2fc8b1071e02b85322ad967394daad72a60f01d4
lejunyang <lejunyang@qq.com>
2023-09-27 11:28:19 +0800

    chore: move test app page from packages/components to root src, rectify dependencies

---
+++ packages/components/index.ts
@@ -0,0 +1,7 @@
+export * from '@lun/core';
+export * from '@lun/utils';
+
+export * from './src/components';
+export * from './src/custom';
+export * from './src/hooks';
+export * from './src/utils';
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,116 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="tsx">
+import { defineBaseInput, defineInput, defineCustomRenderer, defineIcon, registerIconLibrary, svgFillAndSizeDefaultMutator , registerCustomRenderer} from '@lun/components';
+import { reactive, h, defineComponent, onBeforeUnmount, onUnmounted } from "vue";
+
+defineIcon();
+defineBaseInput();
+defineInput();
+defineCustomRenderer();
+
+const TestUnmount = defineComponent({
+	setup() {
+		onBeforeUnmount(() => {
+			console.log('beforeUnmount success');
+		})
+		onUnmounted(() => {
+			console.log('TestUnmount success');
+		})
+		return () => <div>test unmount</div>
+	}
+})
+
+const state = reactive({
+	input: '[a-zA-Z]',
+	baseInput: null,
+	content: (() => h(TestUnmount, { value: 'ttt' })) as any,
+})
+
+const changeContent = () => state.content = `<span style="color: blue">blue</span>`
+
+registerCustomRenderer('custom', {
+	isValidContent: (source) => typeof source === 'string' && source.includes('ljy'),
+	onMounted(source, target, attrs) {
+		console.log(source, 'target', target, attrs);
+		target.innerHTML = source;
+	},
+	onUpdated(source, target, attrs) {
+		console.log(source, 'target', target, attrs);
+		target.innerHTML = source;
+	}
+});
+
+registerIconLibrary({
+	library: 'custom',
+	type: 'vnode',
+	resolver() {
+		return (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="currentColor"
+				class="bi bi-caret-down-fill"
+				viewBox="0 0 16 16">
+				<path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+			</svg>
+		);
+	},
+});
+
+registerIconLibrary({
+	library: 'font-awesome',
+	type: 'html-url',
+	resolver: (name) => {
+		const filename = name.replace(/^fa[rbs]-/, '');
+		let folder = 'regular';
+		if (name.substring(0, 4) === 'fas-') folder = 'solid';
+		if (name.substring(0, 4) === 'fab-') folder = 'brands';
+		return `https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.1/svgs/${folder}/${filename}.svg`;
+	},
+	mutator: ((svg: string) => {
+		return svgFillAndSizeDefaultMutator(svg as string);
+	}) as any,
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div>
+		<a href="https://vitejs.dev" target="_blank">
+			<img src="/vite.svg" class="logo" alt="Vite logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+		</a>
+		<l-icon name="clear" />
+		<l-icon name="sss" library="custom" />
+
+		<l-icon library="font-awesome" name="far-bell" />
+		<l-icon library="font-awesome" name="fas-archive" />
+		<l-icon library="font-awesome" name="fab-apple" />
+
+		<l-input :value="state.input" @update="state.input = $event.detail" />
+
+		<l-base-input v-model="state.baseInput" style="color: red" class="i" type="number-text" />
+		<l-base-input :value="state.baseInput" @update="state.baseInput = $event.detail" style="color: red" class="i" type="number" inputmode="numeric" />
+		baseInput: {{ state.baseInput }}
+
+		<input type="number" @click="changeContent" />
+
+		<l-custom-renderer :content="state.content" />
+	</div>
 </template>
 
-<style scoped>
+<style lang="scss">
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+	filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+	filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
