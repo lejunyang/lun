c0526a6a3e813f34062d4fc2a8e18880ccc5f916
lejunyang <lejunyang@qq.com>
2023-09-27 11:28:19 +0800

    chore: move test app page from packages/components to root src, rectify dependencies

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,3 @@
-declare const __DEV__: string
+declare const __DEV__: string;
+declare module '*.scss';
+declare module '*.scss?inline';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { importCommonStyle } from '@lun/theme';
+import { GlobalStaticConfig } from '@lun/components';
+import "@lun/theme/src/common/element.scss";
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+importCommonStyle();
+console.log('GlobalStaticConfig', GlobalStaticConfig);
+
+createApp(App).mount('#app');
