29fdfb1cf14946ffe00225ef407bc9819826a93f
lejunyang <lejunyang@qq.com>
2023-09-28 23:39:47 +0800

    feat: add vitepress

---
+++ src/docs/.vitepress/locales.ts
@@ -0,0 +1,32 @@
+export default {
+  zh: {
+    nav: {
+      home: '主页',
+      components: '组件',
+    },
+    sidebar: {
+      basic: {
+        menu: 'Basic 基础',
+        button: 'Button 按钮',
+        icon: 'Icon 图标',
+        baseInput: 'BaseInput 基础输入',
+        input: 'Input 输入'
+      }
+    }
+  },
+  en: {
+    nav: {
+      home: 'Home',
+      components: 'Components',
+    },
+    sidebar: {
+      basic: {
+        menu: 'Basic',
+        button: 'Button',
+        icon: 'Icon',
+        baseInput: 'BaseInput',
+        input: 'Input'
+      }
+    }
+  },
+}
---
+++ src/docs/.vitepress/theme/index.ts
@@ -0,0 +1,22 @@
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue';
+import Theme from 'vitepress/theme';
+import { importCommonStyle } from '@lun/theme';
+import { GlobalStaticConfig, defineAllComponents } from '@lun/components';
+import '@lun/theme/scss/common/index.scss';
+import './style.css';
+
+export default {
+  extends: Theme,
+  Layout: () => {
+    return h(Theme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    });
+  },
+  enhanceApp({ app, router, siteData }) {
+    importCommonStyle();
+    console.log('GlobalStaticConfig', GlobalStaticConfig);
+    defineAllComponents();
+    // ...
+  },
+};
