de8fa43e51bf34abfea0d97f8b589a72f6785fe0
lejunyang <lejunyang@qq.com>
2023-10-02 22:52:54 +0800

    feat(components): support dynamicStyles

--- packages/components/src/components/config/config.context.ts
+++ packages/components/src/components/config/config.context.ts
@@ -1,17 +1,31 @@
-import { inject, provide, reactive } from 'vue';
+import { ComponentInternalInstance, inject, provide, reactive } from 'vue';
 import { iconRegistryMap } from '../icon/icon.registry';
+import { ComponentKey, components } from './config.static';
 
 export const CONTEXT_CONFIG_KEY = Symbol(__DEV__ ? 'l-context-config-key' : '');
 
 export const GlobalContextConfig = reactive({
-	iconRegistryMap,
+  iconRegistryMap,
+  dynamicStyles: (() => {
+    const original = components.reduce((result, name) => {
+      result[name] = [];
+      return result;
+    }, {} as Record<'common' | ComponentKey, ((vm: ComponentInternalInstance) => string)[]>);
+    const commonStyles = [] as ((vm: ComponentInternalInstance) => string)[];
+    return new Proxy(original, {
+      get(target, p, receiver) {
+        if (p === 'common') return commonStyles;
+        return [...commonStyles, ...Reflect.get(target, p, receiver)].filter(Boolean);
+      },
+    });
+  })(),
 });
 
 export type TGlobalContextConfig = typeof GlobalContextConfig;
 export type kTGlobalContextConfig = keyof TGlobalContextConfig;
 
 export function provideContextConfig(config: TGlobalContextConfig) {
-	provide(CONTEXT_CONFIG_KEY, config);
+  provide(CONTEXT_CONFIG_KEY, config);
 }
 
 export function useContextConfig(): TGlobalContextConfig;
@@ -20,6 +34,6 @@ export function useContextConfig<K extends kTGlobalContextConfig>(key: K): TGlob
  * return a specific part of current effect config, if no key provided, return the whole config object
  */
 export function useContextConfig(key?: kTGlobalContextConfig) {
-	const result = inject(CONTEXT_CONFIG_KEY, GlobalContextConfig);
-	return key ? result[key] : result;
+  const result = inject(CONTEXT_CONFIG_KEY, GlobalContextConfig);
+  return key ? result[key] : result;
 }
