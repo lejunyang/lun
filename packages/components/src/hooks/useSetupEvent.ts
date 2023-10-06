73226c926d91a18b4c7a8270bd57c89aba2567a8
lejunyang <lejunyang@qq.com>
2023-10-06 10:39:37 +0800

    feat(components/hooks): add toParentWhen feature

--- packages/components/src/hooks/useSetupEvent.ts
+++ packages/components/src/hooks/useSetupEvent.ts
@@ -8,25 +8,26 @@ export type Events = Record<string, (...args: any[]) => void>;
  * Create an event context. When current component emits an event, it will bubble that event, which means it will trigger all the context parent's listeners
  * @param events
  */
-export function useSetupEvent(events?: Events) {
-	const ctx = getCurrentInstance()!;
-	if (!ctx || !ctx.event) {
-		throw new Error('Do not use `useSetupEvent` outside the defineCustomElement setup function scope');
-	}
-	const { event } = ctx;
+export function useSetupEvent(events?: Events, options?: { toParentWhen?: () => boolean }) {
+  const { toParentWhen } = options || {};
+  const ctx = getCurrentInstance()!;
+  if (!ctx || !ctx.event) {
+    throw new Error(`Do not use 'useSetupEvent' outside the lun's defineCustomElement setup function scope`);
+  }
+  const { event } = ctx;
 
-	const parentSubscribedEvents = inject<Events>(EVENT_PROVIDER_KEY);
-	let unsubscribes: Function[] = [];
-	const currentProvide: Events = {};
-	Object.keys({ ...ctx.emitsOptions }).forEach((k) => {
-		currentProvide[k] = (...args) => {
-			if (events?.[k] instanceof Function) events[k](...args);
-			if (parentSubscribedEvents?.[k] instanceof Function) parentSubscribedEvents[k](...args);
-		};
-		unsubscribes.push(event.on(k, currentProvide[k]));
-	});
-	provide(EVENT_PROVIDER_KEY, currentProvide);
-	onBeforeUnmount(() => unsubscribes.forEach((fn) => fn()));
+  const parentSubscribedEvents = inject<Events>(EVENT_PROVIDER_KEY);
+  let unsubscribes: Function[] = [];
+  const currentProvide: Events = {};
+  Object.keys({ ...ctx.emitsOptions }).forEach((k) => {
+    currentProvide[k] = (...args) => {
+      if (events?.[k] instanceof Function) events[k](...args);
+      if (parentSubscribedEvents?.[k] instanceof Function) parentSubscribedEvents[k](...args);
+    };
+    unsubscribes.push(toParentWhen ? event.when(k, toParentWhen, currentProvide[k]) : event.on(k, currentProvide[k]));
+  });
+  provide(EVENT_PROVIDER_KEY, currentProvide);
+  onBeforeUnmount(() => unsubscribes.forEach((fn) => fn()));
 }
 
 export const CONTEXT_EVENT_PROVIDER_KEY = Symbol(__DEV__ ? 'l-context-event-provider-key' : '');
@@ -36,18 +37,22 @@ export const CONTEXT_EVENT_PROVIDER_KEY = Symbol(__DEV__ ? 'l-context-event-prov
  * Comparing to useSetupEvent, it doesn't bubble the event
  * @param events
  */
-export function useSetupContextEvent(events?: Events) {
-	const ctx = getCurrentInstance()!;
-	if (!ctx || !ctx.event) {
-		throw new Error('Do not use `useSetupContextEvent` outside the defineCustomElement setup function scope');
-	}
-	const { event } = ctx;
+export function useSetupContextEvent(events?: Events, options?: { toParentWhen?: () => boolean }) {
+  const { toParentWhen } = options || {};
+  const ctx = getCurrentInstance()!;
+  if (!ctx || !ctx.event) {
+    throw new Error(`Do not use 'useSetupContextEvent' outside the lun's defineCustomElement setup function scope`);
+  }
+  const { event } = ctx;
 
-	const parentSubscribedEvents = inject<Events>(CONTEXT_EVENT_PROVIDER_KEY);
-	let unsubscribes: Function[] = [];
-	Object.keys({ ...ctx.emitsOptions }).forEach((k) => {
-		if (parentSubscribedEvents?.[k] instanceof Function) unsubscribes.push(event.on(k, parentSubscribedEvents[k]));
-	});
-	provide(CONTEXT_EVENT_PROVIDER_KEY, events);
-	onBeforeUnmount(() => unsubscribes.forEach((fn) => fn()));
+  const parentSubscribedEvents = inject<Events>(CONTEXT_EVENT_PROVIDER_KEY);
+  let unsubscribes: Function[] = [];
+  Object.keys({ ...ctx.emitsOptions }).forEach((k) => {
+    if (parentSubscribedEvents?.[k] instanceof Function)
+      unsubscribes.push(
+        toParentWhen ? event.when(k, toParentWhen, parentSubscribedEvents[k]) : event.on(k, parentSubscribedEvents[k])
+      );
+  });
+  provide(CONTEXT_EVENT_PROVIDER_KEY, events);
+  onBeforeUnmount(() => unsubscribes.forEach((fn) => fn()));
 }
