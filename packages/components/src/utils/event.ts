38310501ea0aa43c01f6d2960d802656949b5558
lejunyang <lejunyang@qq.com>
2023-10-06 10:39:37 +0800

    feat(components/hooks): add toParentWhen feature

--- packages/components/src/utils/event.ts
+++ packages/components/src/utils/event.ts
@@ -1,87 +1,91 @@
-import type { EmitsOptions, ObjectEmitsOptions } from 'vue';
+import { isFunction } from '@lun/utils';
+import type { ObjectEmitsOptions } from 'vue';
 
 export type SimpleFunction = (...args: any[]) => any;
 export type PlainObject = Record<string, any>;
 
 type Listener = SimpleFunction & { fn?: any };
-// type ListenName = string | symbol;
 
 export function createPlainEvent<
-	E extends ObjectEmitsOptions | readonly string[],
-	ListenName = E extends readonly string[] ? E[number] : E extends ObjectEmitsOptions ? keyof E : never
->(emitsOptions?: E | null) {
-	const getListenMap = (() => {
-		let events: Map<ListenName, Listener[]>;
-		return () => {
-			if (!events) {
-				events = new Map<ListenName, Listener[]>();
-			}
-			return events;
-		};
-	})();
+  E extends ObjectEmitsOptions | readonly string[],
+  ListenName = E extends readonly string[] ? E[number] : E extends ObjectEmitsOptions ? keyof E : never
+>(_emitsOptions?: E | null) {
+  const getListenMap = (() => {
+    let events: Map<ListenName, Listener[]>;
+    return () => {
+      if (!events) {
+        events = new Map<ListenName, Listener[]>();
+      }
+      return events;
+    };
+  })();
 
-	let hasListener = false;
+  const listenerCondition = new WeakMap<Function, () => boolean>();
 
-	const event = {
-		on: (listenName: ListenName, fn: SimpleFunction) => {
-			hasListener = true;
+  let hasListener = false;
 
-			const listenMap = getListenMap();
-			const map = listenMap.get(listenName);
-			if (!!map) {
-				map.push(fn);
-			} else {
-				listenMap.set(listenName, [fn]);
-			}
-			return () => event.off(listenName, fn);
-		},
-		once: (listenName: ListenName, fn: SimpleFunction) => {
-			hasListener = true;
+  const event = {
+    on: (listenName: ListenName, fn: SimpleFunction) => {
+      hasListener = true;
 
-			const on: Listener = (...args: any[]) => {
-				event.off(listenName, fn);
-				fn(...args);
-			};
-			on.fn = fn;
-			event.on(listenName, on);
-			return () => event.off(listenName, on);
-		},
-		off: (listenName: ListenName, fn?: SimpleFunction) => {
-			const listenMap = getListenMap();
+      const listenMap = getListenMap();
+      const map = listenMap.get(listenName);
+      if (map) {
+        map.push(fn);
+      } else {
+        listenMap.set(listenName, [fn]);
+      }
+      return () => event.off(listenName, fn);
+    },
+    when: (listenName: ListenName, condition: () => boolean, fn: SimpleFunction) => {
+      if (isFunction(condition)) listenerCondition.set(fn, condition);
+      return event.on(listenName, fn);
+    },
+    once: (listenName: ListenName, fn: SimpleFunction) => {
+      hasListener = true;
 
-			const listeners = listenMap.get(listenName);
-			if (!listeners) {
-				return;
-			}
+      const on: Listener = (...args: any[]) => {
+        event.off(listenName, fn);
+        fn(...args);
+      };
+      on.fn = fn; // record that, so we can use that fn to off manually
+      event.on(listenName, on);
+      return () => event.off(listenName, on);
+    },
+    off: (listenName: ListenName, fn?: SimpleFunction) => {
+      const listenMap = getListenMap();
 
-			/*移除listenName的所有监听器*/
-			if (!fn) {
-				listenMap.set(listenName, []);
-				return;
-			}
+      const listeners = listenMap.get(listenName);
+      if (!listeners) {
+        return;
+      }
 
-			for (let i = 0; i < listeners.length; i++) {
-				const listener = listeners[i];
-				if (fn === listener || (!!listener.fn && fn === listener.fn)) {
-					listeners.splice(i, 1);
-					break;
-				}
-			}
-		},
-		emit: (listenName: ListenName, ...args: any[]) => {
-			const listeners = [...(getListenMap().get(listenName) || [])];
-			if (!!listeners) {
-				listeners.forEach((listener) => listener(...args));
-			}
-		},
-		clear: () => {
-			if (hasListener) {
-				getListenMap().clear();
-			}
-		},
-	};
+      // clear all
+      if (!fn) {
+        listenMap.set(listenName, []);
+        return;
+      }
 
-	return event;
+      const index = listeners.findIndex((listener) => fn === listener || (listener.fn && fn === listener.fn));
+      if (index !== -1) listeners.splice(index, 1);
+    },
+    emit: (listenName: ListenName, ...args: any[]) => {
+      const listeners = [...(getListenMap().get(listenName) || [])];
+      if (listeners) {
+        listeners.forEach((listener) => {
+          const condition = listenerCondition.get(listener);
+          if (!condition || condition()) listener(...args);
+        });
+      }
+    },
+    clear: () => {
+      if (hasListener) {
+        getListenMap().clear();
+      }
+    },
+  };
+
+  return event;
 }
 
 export type PlainEvent = ReturnType<typeof createPlainEvent>;
@@ -101,45 +105,45 @@ export type NotBindEvents = Record<string, ['addEventListener' | 'removeEventLis
  * @returns
  */
 export function delegateEvent(
-	sourceEl: HTMLElement | null,
-	targetElGetter: HTMLElement | null | (() => HTMLElement | null),
-	events: string[] | Set<string>,
-	options?: { fallbackToSourceWhenNoTarget?: boolean }
+  sourceEl: HTMLElement | null,
+  targetElGetter: HTMLElement | null | (() => HTMLElement | null),
+  events: string[] | Set<string>,
+  options?: { fallbackToSourceWhenNoTarget?: boolean }
 ) {
-	if (!sourceEl || !targetElGetter || !Array.isArray(events)) return;
-	const { fallbackToSourceWhenNoTarget } = options || {};
-	const originalSourceAdd = sourceEl.addEventListener.bind(sourceEl);
-	const originalSourceRemove = sourceEl.removeEventListener.bind(sourceEl);
-	let target = targetElGetter instanceof Function ? targetElGetter() : targetElGetter;
-	const eventSet = new Set(events);
-	const notBindEvents: NotBindEvents = {};
+  if (!sourceEl || !targetElGetter || !Array.isArray(events)) return;
+  const { fallbackToSourceWhenNoTarget } = options || {};
+  const originalSourceAdd = sourceEl.addEventListener.bind(sourceEl);
+  const originalSourceRemove = sourceEl.removeEventListener.bind(sourceEl);
+  let target = targetElGetter instanceof Function ? targetElGetter() : targetElGetter;
+  const eventSet = new Set(events);
+  const notBindEvents: NotBindEvents = {};
 
-	sourceEl.addEventListener = function (type: EventType, listener: EventListener, options?: EventOptions) {
-		if (!target && targetElGetter instanceof Function) {
-			target = targetElGetter();
-		}
-		if (eventSet.has(type)) {
-			if (target) target.addEventListener(type, listener, options);
-			else if (fallbackToSourceWhenNoTarget) originalSourceAdd(type, listener, options);
-			else {
-				if (!notBindEvents[type]) notBindEvents[type] = [];
-				notBindEvents[type].push(['addEventListener', listener, options]);
-			}
-		} else originalSourceAdd(type, listener, options);
-	};
-	sourceEl.removeEventListener = function (type: EventType, listener: EventListener, options?: EventOptions) {
-		if (!target && targetElGetter instanceof Function) {
-			target = targetElGetter();
-		}
-		if (eventSet.has(type)) {
-			if (target) target.removeEventListener(type, listener, options);
-			else if (fallbackToSourceWhenNoTarget) originalSourceRemove(type, listener, options);
-			else {
-				if (!notBindEvents[type]) notBindEvents[type] = [];
-				notBindEvents[type].push(['removeEventListener', listener, options]);
-			}
-		} else originalSourceRemove(type, listener, options);
-	};
+  sourceEl.addEventListener = function (type: EventType, listener: EventListener, options?: EventOptions) {
+    if (!target && targetElGetter instanceof Function) {
+      target = targetElGetter();
+    }
+    if (eventSet.has(type)) {
+      if (target) target.addEventListener(type, listener, options);
+      else if (fallbackToSourceWhenNoTarget) originalSourceAdd(type, listener, options);
+      else {
+        if (!notBindEvents[type]) notBindEvents[type] = [];
+        notBindEvents[type].push(['addEventListener', listener, options]);
+      }
+    } else originalSourceAdd(type, listener, options);
+  };
+  sourceEl.removeEventListener = function (type: EventType, listener: EventListener, options?: EventOptions) {
+    if (!target && targetElGetter instanceof Function) {
+      target = targetElGetter();
+    }
+    if (eventSet.has(type)) {
+      if (target) target.removeEventListener(type, listener, options);
+      else if (fallbackToSourceWhenNoTarget) originalSourceRemove(type, listener, options);
+      else {
+        if (!notBindEvents[type]) notBindEvents[type] = [];
+        notBindEvents[type].push(['removeEventListener', listener, options]);
+      }
+    } else originalSourceRemove(type, listener, options);
+  };
 
-	return { addEventListener: originalSourceAdd, removeEventListener: originalSourceRemove, notBindEvents };
+  return { addEventListener: originalSourceAdd, removeEventListener: originalSourceRemove, notBindEvents };
 }
