c7ff1d88e4ab5276f85e22588a2c36ed03a408be
lejunyang <lejunyang@qq.com>
2023-10-07 21:59:09 +0800

    refactor: extract common codes for custom-renderer and input

--- packages/components/src/components/input/BaseInput.tsx
+++ packages/components/src/components/input/BaseInput.tsx
@@ -1,68 +1,58 @@
 import { useInput, useSetupEdit } from '@lun/core';
 import { defineSSRCustomFormElement } from 'custom';
-import { setDefaultsForPropOptions } from 'utils';
-import { editStateProps, inputProps } from 'common';
+import { createDefineComp, getCommonCompOptions, setDefaultsForPropOptions } from 'utils';
 import { GlobalStaticConfig } from 'config';
 import { useSetupContextEvent, useVModelCompatible } from 'hooks';
+import { baseInputProps } from './type';
 
 export const BaseInput = defineSSRCustomFormElement({
-	name: GlobalStaticConfig.nameMap['base-input'],
-	inheritAttrs: false,
-	props: {
-		...editStateProps,
-		...setDefaultsForPropOptions(inputProps, GlobalStaticConfig.defaultProps['base-input']),
-	},
-	styles: GlobalStaticConfig.computedStyles['base-input'],
-	emits: ['update', 'enterDown'],
-	setup(props, { emit, attrs }) {
-		useSetupContextEvent();
-		const [editComputed] = useSetupEdit();
-		const [updateVModel] = useVModelCompatible();
+  ...getCommonCompOptions('base-input'),
+  props: setDefaultsForPropOptions(baseInputProps, GlobalStaticConfig.defaultProps['base-input']),
+  inheritAttrs: false,
+  emits: ['update', 'enterDown'],
+  setup(props, { emit, attrs }) {
+    useSetupContextEvent();
+    const [editComputed] = useSetupEdit();
+    const [updateVModel] = useVModelCompatible();
 
-		const [inputHandlers] = useInput(() => ({
-			...attrs,
-			...props,
-			onChange: (val) => {
-				updateVModel(val);
-				emit('update', val);
-			},
-			onEnterDown(e) {
-				emit('enterDown', e);
-			},
-		}));
+    const [inputHandlers] = useInput(() => ({
+      ...attrs,
+      ...props,
+      onChange: (val) => {
+        updateVModel(val);
+        emit('update', val);
+      },
+      onEnterDown(e) {
+        emit('enterDown', e);
+      },
+    }));
 
-		return () => (
-			<input
-				{...attrs}
-				type={props.type === 'number-text' ? 'tel' : props.type}
-				part="input"
-				class={['l-base-input']}
-				value={props.value}
-				placeholder={props.placeholder}
-				disabled={editComputed.value.disabled}
-				readonly={editComputed.value.readonly}
-				{...inputHandlers}
-			/>
-		);
-	},
+    return () => (
+      <input
+        {...attrs}
+        type={props.type === 'number-text' ? 'tel' : props.type}
+        part="input"
+        class={['l-base-input']}
+        value={props.value}
+        placeholder={props.placeholder}
+        disabled={editComputed.value.disabled}
+        readonly={editComputed.value.readonly}
+        {...inputHandlers}
+      />
+    );
+  },
 });
 
-export function defineBaseInput(name?: string) {
-	name ||= GlobalStaticConfig.nameMap['base-input'];
-	if (!customElements.get(name)) {
-		GlobalStaticConfig.actualNameMap['base-input'].add(name);
-		customElements.define(name, BaseInput);
-	}
-}
-
 declare module 'vue' {
-	export interface GlobalComponents {
-		LBaseInput: typeof BaseInput;
-	}
+  export interface GlobalComponents {
+    LBaseInput: typeof BaseInput;
+  }
 }
 
 declare global {
-	interface HTMLElementTagNameMap {
-		'l-base-input': typeof BaseInput;
-	}
+  interface HTMLElementTagNameMap {
+    'l-base-input': typeof BaseInput;
+  }
 }
+
+export const defineBaseInput = createDefineComp('base-input', BaseInput);
--- packages/components/src/components/input/Input.tsx
+++ packages/components/src/components/input/Input.tsx
@@ -1,130 +1,117 @@
-import { PropType, onBeforeUnmount, onUnmounted } from 'vue';
-import { useInput, useSetupEdit, Responsive, useComputedBreakpoints } from '@lun/core';
+import { onBeforeUnmount, onUnmounted } from 'vue';
+import { useInput, useSetupEdit, useComputedBreakpoints } from '@lun/core';
 import { defineSSRCustomFormElement } from 'custom';
-import { setDefaultsForPropOptions } from 'utils';
+import { createDefineComp, getCommonCompOptions, setDefaultsForPropOptions } from 'utils';
 import { useVModelCompatible, useValueModel } from 'hooks';
-import { editStateProps, inputProps } from 'common';
 import { GlobalStaticConfig } from 'config';
+import { inputProps } from './type';
 
 export const Input = defineSSRCustomFormElement({
-	name: GlobalStaticConfig.nameMap.input,
-	inheritAttrs: false,
-	props: {
-		...editStateProps,
-		label: { type: String },
-		labelType: { type: String as PropType<'float'> },
-		size: { type: [String, Object] as PropType<Responsive<'1' | '2' | '3'>>, default: '1' },
-		showLengthInfo: { type: Boolean },
-		showClearIcon: { type: Boolean },
-		...setDefaultsForPropOptions(inputProps, GlobalStaticConfig.defaultProps.input),
-	},
-	styles: GlobalStaticConfig.computedStyles.input,
-	emits: ['update', 'enterDown'],
-	setup(props, { emit, attrs }) {
-		const valueModel = useValueModel(props);
-		const [updateVModel] = useVModelCompatible();
-		const [editComputed] = useSetupEdit();
+  ...getCommonCompOptions('input'),
+  props: setDefaultsForPropOptions(inputProps, GlobalStaticConfig.defaultProps.input),
+  inheritAttrs: false,
+  emits: ['update', 'enterDown'],
+  setup(props, { emit, attrs }) {
+    const valueModel = useValueModel(props);
+    const [updateVModel] = useVModelCompatible();
+    const [editComputed] = useSetupEdit();
 
-		const inputSizeClass = useComputedBreakpoints(() => props.size, 'l-input-size');
+    const inputSizeClass = useComputedBreakpoints(() => props.size, 'l-input-size');
 
-		const [inputHandlers] = useInput(() => ({
-			...props,
-			onChange: (val) => {
-				updateVModel(val);
-				emit('update', val);
-			},
-			onEnterDown(e) {
-				emit('enterDown', e);
-			},
-		}));
-		onBeforeUnmount(() => {
-			console.log('on beforeUnmount');
-		})
-		onUnmounted(() => {
-			console.log('unmmmmmm');
-		})
-		// TODO mouse enter add class to show the clear button.  animation, hide suffix slot(render both, z-index?)
-		return () => (
-			<span
-				part="root"
-				class={[
-					'l-input-root',
-					'l-input-variant-surface',
-					inputSizeClass.value,
-					valueModel.value ? 'l-input-not-empty' : 'l-input-empty',
-				]}>
-				<div class="l-input-slot l-input-addon-before" part="addon-before">
-					<slot name="addon-before"></slot>
-				</div>
-				<label class="l-input-label" part="label">
-					<div class="l-input-slot l-input-prefix" part="prefix">
-						<slot name="prefix"></slot>
-						{props.labelType === 'float' && (
-							<div class="l-input-label l-input-float-label" part="float-label">
-								{props.label}
-								<div class="l-input-float-label-back">{props.label}</div>
-							</div>
-						)}
-					</div>
-					<span style="position: relative">
-						{/* render when value is defined，in case it covers float label and placeholder */}
-						{valueModel.value && (
-							<div class="l-input l-input-custom-renderer">
-								<slot name="renderer"></slot>
-							</div>
-						)}
-						<input
-							{...attrs}
-							type={props.type}
-							id="input"
-							part="input"
-							class={['l-input']}
-							value={valueModel.value}
-							placeholder={props.placeholder}
-							disabled={editComputed.value.disabled}
-							readonly={editComputed.value.readonly}
-							{...inputHandlers}
-						/>
-					</span>
-					<div class="l-input-back" />
-					<div
-						class={['l-input-slot l-input-suffix', props.showClearIcon && 'l-input-suffix-with-clear']}
-						part="suffix">
-						{props.showClearIcon && <span class="l-input-clear-icon">x</span>}
-						<slot name="suffix">
-							<span class="l-input-clear-icon">x</span>
-						</slot>
-					</div>
-					{props.maxLength! >= 0 && (
-						<div class="l-input-length-info">
-							{valueModel.value?.length || '0'}/{props.maxLength}
-						</div>
-					)}
-				</label>
-				<div class="l-input-slot l-input-addonAfter" part="addon-after">
-					<slot name="addon-after"></slot>
-				</div>
-			</span>
-		);
-	},
+    const [inputHandlers] = useInput(() => ({
+      ...props,
+      onChange: (val) => {
+        updateVModel(val);
+        emit('update', val);
+      },
+      onEnterDown(e) {
+        emit('enterDown', e);
+      },
+    }));
+    onBeforeUnmount(() => {
+      console.log('on beforeUnmount');
+    });
+    onUnmounted(() => {
+      console.log('unmmmmmm');
+    });
+    // TODO mouse enter add class to show the clear button.  animation, hide suffix slot(render both, z-index?)
+    return () => (
+      <span
+        part="root"
+        class={[
+          'l-input-root',
+          'l-input-variant-surface',
+          inputSizeClass.value,
+          valueModel.value ? 'l-input-not-empty' : 'l-input-empty',
+        ]}
+      >
+        <div class="l-input-slot l-input-addon-before" part="addon-before">
+          <slot name="addon-before"></slot>
+        </div>
+        <label class="l-input-label" part="label">
+          <div class="l-input-slot l-input-prefix" part="prefix">
+            <slot name="prefix"></slot>
+            {props.labelType === 'float' && (
+              <div class="l-input-label l-input-float-label" part="float-label">
+                {props.label}
+                <div class="l-input-float-label-back">{props.label}</div>
+              </div>
+            )}
+          </div>
+          <span style="position: relative">
+            {/* render when value is defined，in case it covers float label and placeholder */}
+            {valueModel.value && (
+              <div class="l-input l-input-custom-renderer">
+                <slot name="renderer"></slot>
+              </div>
+            )}
+            <input
+              {...attrs}
+              type={props.type}
+              id="input"
+              part="input"
+              class={['l-input']}
+              value={valueModel.value}
+              placeholder={props.placeholder}
+              disabled={editComputed.value.disabled}
+              readonly={editComputed.value.readonly}
+              {...inputHandlers}
+            />
+          </span>
+          <div class="l-input-back" />
+          <div
+            class={['l-input-slot l-input-suffix', props.showClearIcon && 'l-input-suffix-with-clear']}
+            part="suffix"
+          >
+            {props.showClearIcon && <span class="l-input-clear-icon">x</span>}
+            <slot name="suffix">
+              <span class="l-input-clear-icon">x</span>
+            </slot>
+          </div>
+          {props.maxLength! >= 0 && (
+            <div class="l-input-length-info">
+              {valueModel.value?.length || '0'}/{props.maxLength}
+            </div>
+          )}
+        </label>
+        <div class="l-input-slot l-input-addonAfter" part="addon-after">
+          <slot name="addon-after"></slot>
+        </div>
+      </span>
+    );
+  },
 });
 
-export function defineInput(name?: string) {
-	name = GlobalStaticConfig.nameMap.input;
-	if (!customElements.get(name)) {
-		GlobalStaticConfig.actualNameMap['input'].add(name);
-		customElements.define(name, Input);
-	}
-}
-
 declare module 'vue' {
-	export interface GlobalComponents {
-		LInput: typeof Input;
-	}
+  export interface GlobalComponents {
+    LInput: typeof Input;
+  }
 }
 
 declare global {
-	interface HTMLElementTagNameMap {
-		'l-input': typeof Input;
-	}
+  interface HTMLElementTagNameMap {
+    'l-input': typeof Input;
+  }
 }
+
+export const defineInput = createDefineComp('input', Input);
