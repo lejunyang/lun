59bc5d3476cc3ac5dc0979df71dbe1cde6440eab
lejunyang <lejunyang@qq.com>
2023-10-10 00:33:05 +0800

    feat(components/popover):  popover first edition

--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -3,6 +3,7 @@ import { defineCheckbox, defineCheckboxGroup } from './checkbox';
 import { defineCustomRenderer } from './custom-renderer';
 import { defineIcon } from './icon';
 import { defineBaseInput, defineInput } from './input';
+import { definePopover } from './popover';
 import { defineRadio, defineRadioGroup } from './radio';
 import { defineSpin } from './spin';
 
@@ -15,6 +16,7 @@ export function defineAllComponents() {
   defineIcon();
   defineBaseInput();
   defineInput();
+  definePopover();
   defineRadio();
   defineRadioGroup();
   defineSpin();
@@ -26,6 +28,7 @@ export * from './config';
 export * from './checkbox';
 export * from './custom-renderer';
 export * from './input';
+export * from './popover';
 export * from './radio';
 export * from './icon';
 export * from './spin';
