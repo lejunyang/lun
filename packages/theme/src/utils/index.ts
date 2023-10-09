6c4093f746bcf29c87e90cfaec2f58ed230bc2bc
lejunyang <lejunyang@qq.com>
2023-10-09 16:36:32 +0800

    chore: move processStringStyle from theme package to components package

--- packages/theme/src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import { GlobalStaticConfig, supportCSSStyleSheet } from '@lun/components';
-
-export function processStringStyle(style: string) {
-  if (supportCSSStyleSheet() && GlobalStaticConfig.preferCSSStyleSheet) {
-    const sheet = new CSSStyleSheet();
-    sheet.replaceSync(style);
-    return sheet;
-  } else return style;
-}
