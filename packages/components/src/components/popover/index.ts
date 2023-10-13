7b52936b263de8c35ebc097a9443d4eebfcceade
lejunyang <lejunyang@qq.com>
2023-10-13 10:32:53 +0800

    feat(components/popover): add basic styles, add 'show' prop for manual control, add 'placement' prop

--- packages/components/src/components/popover/index.ts
+++ packages/components/src/components/popover/index.ts
@@ -1,2 +1,6 @@
+import { createImportStyle } from '../../utils';
+import style from './basic.scss?inline';
+
 export * from './Popover';
 export type { PopoverProps } from './type';
+export const importPopoverStyle = createImportStyle('popover', style);
--- packages/components/src/components/popover/type.ts
+++ packages/components/src/components/popover/type.ts
@@ -1,12 +1,21 @@
+import { Placement } from '@floating-ui/vue';
 import type { PopoverTrigger } from '@lun/core';
 import type { ExtractPropTypes, PropType } from 'vue';
 
 export const popoverProps = {
-  type: { type: String as PropType<'popover' | 'dialog' | 'fixed' | 'teleportFixed'> },
+  show: { type: Boolean, default: undefined },
+  type: { type: String as PropType<'popover' | 'dialog' | 'fixed' | 'body-fixed'> },
   content: {},
+  placement: { type: String as PropType<Placement> },
   showDelay: { type: Number },
   hideDelay: { type: Number },
   triggers: { type: [String, Array] as PropType<PopoverTrigger | PopoverTrigger[]> },
 };
 
 export type PopoverProps = ExtractPropTypes<typeof popoverProps>;
+
+declare module 'vue' {
+  export interface IntrinsicElementAttributes {
+    'l-popover': PopoverProps & HTMLAttributes
+  }
+}
