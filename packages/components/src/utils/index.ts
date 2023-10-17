a255becd73497e6ff6ce24cbd197310a703bd590
lejunyang <lejunyang@qq.com>
2023-10-17 14:44:02 +0800

    feat(components/utils): toGetterDescriptors

--- packages/components/src/utils/index.ts
+++ packages/components/src/utils/index.ts
@@ -1,4 +1,23 @@
+import { MaybeRefLikeOrGetter, unrefOrGet } from '@lun/core';
+
 export * from './component';
 export * from './console';
 export * from './style';
 export * from './vueUtils';
+
+export function toGetterDescriptors<
+  M extends MaybeRefLikeOrGetter<Record<string, unknown>>,
+  T = M extends MaybeRefLikeOrGetter<infer V> ? V : any,
+  K extends keyof T = keyof T,
+  PM extends Partial<Record<K, string>> = Partial<Record<K, string>>,
+  >(obj: M, propertiesMap: PM): Record<PM[keyof PM] & {}, PropertyDescriptor> {
+  const descriptors = {} as any;
+  for (const [key, value] of Object.entries(propertiesMap)) {
+    descriptors[value as any] = {
+      get() {
+        return unrefOrGet(obj)?.[key];
+      },
+    };
+  }
+  return descriptors;
+}
