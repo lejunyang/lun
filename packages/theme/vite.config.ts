b1b5f424916f2cfaffeeec41635a74b8a5b04795
lejunyang <lejunyang@qq.com>
2023-10-19 23:43:50 +0800

    chore(theme): change script of generating color file

--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -29,12 +29,12 @@ export default defineConfig({
     }
   },
 	css: {
-		// css预处理器
 		preprocessorOptions: {
 			scss: {
-				// 引入 mixin.scss 这样就可以在全局中使用 mixin.scss中预定义的变量了
-				// 给导入的路径最后加上 ;
-				// additionalData: '@import "@/assets/style/mixin.scss";',
+				additionalData: `
+				@use "@/scss/mixins/config";
+				@use "@/scss/mixins/bem" as *;
+				`,
 			},
 		},
 	},
