39a1986040c11626a3778012a01206f47e8ed89c
lejunyang <lejunyang@qq.com>
2023-10-22 22:40:45 +0800

    feat(utils): add toNoneNilSet

--- packages/utils/src/array.ts
+++ packages/utils/src/array.ts
@@ -1,3 +1,12 @@
 export function toArrayIfNotNil<T>(target: T): T extends any[] ? T : T extends null | undefined ? unknown[] : T[] {
   return (Array.isArray(target) ? target : target == null ? [] : [target]) as any;
 }
+
+export function toNoneNilSet<T>(...args: T[]): T extends any[] ? Set<T[number]> : Set<T> {
+  const result = new Set();
+  args.forEach((arg) => {
+    if (Array.isArray(arg)) arg.forEach((item) => item != null && result.add(item));
+    else if (arg != null) result.add(arg);
+  });
+  return result as any;
+}
