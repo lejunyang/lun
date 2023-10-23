e91f84452d0ac7edfe5c79fe1ef22259c2fb495c
lejunyang <lejunyang@qq.com>
2023-10-23 17:14:32 +0800

    feat(utils/toNoneNilSet): support iterable

--- packages/utils/src/array.ts
+++ packages/utils/src/array.ts
@@ -1,11 +1,24 @@
+import { isFunction } from './is';
+
 export function toArrayIfNotNil<T>(target: T): T extends any[] ? T : T extends null | undefined ? unknown[] : T[] {
   return (Array.isArray(target) ? target : target == null ? [] : [target]) as any;
 }
 
-export function toNoneNilSet<T>(...args: T[]): T extends any[] ? Set<T[number]> : Set<T> {
+export function isIterable<T>(target: any): target is Iterable<T> {
+  return target != null && isFunction(target[Symbol.iterator]);
+}
+
+export type ExtractIterable<T> = T extends Iterable<infer E> ? E : never;
+
+// https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type
+export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
+
+export function toNoneNilSet<T extends (Iterable<any> | null | undefined)[]>(
+  ...args: T
+): Set<ExtractIterable<T[number]>> {
   const result = new Set();
   args.forEach((arg) => {
-    if (Array.isArray(arg)) arg.forEach((item) => item != null && result.add(item));
+    if (isIterable(arg)) Array.from(arg).forEach((item) => item != null && result.add(item));
     else if (arg != null) result.add(arg);
   });
   return result as any;
