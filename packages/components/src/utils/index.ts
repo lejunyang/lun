3f37ed958f8f99c9e455de3ca35bf99a726367a1
lejunyang <lejunyang@qq.com>
2023-10-24 10:01:30 +0800

    feat(components/input): expose focus and blur method to input CE

--- packages/components/src/utils/index.ts
+++ packages/components/src/utils/index.ts
@@ -6,16 +6,16 @@ export * from './style';
 export * from './vueUtils';
 
 export function toGetterDescriptors<
-  M extends MaybeRefLikeOrGetter<Record<string, unknown>>,
+  M extends MaybeRefLikeOrGetter<{}>,
   T = M extends MaybeRefLikeOrGetter<infer V> ? V : any,
   K extends keyof T = keyof T,
-  PM extends Partial<Record<K, string>> = Partial<Record<K, string>>,
-  >(obj: M, propertiesMap: PM): Record<PM[keyof PM] & {}, PropertyDescriptor> {
+  PM extends Partial<Record<K, string>> = Partial<Record<K, string>>
+>(obj: M, propertiesMap: PM): Record<PM[keyof PM] & {}, PropertyDescriptor> {
   const descriptors = {} as any;
   for (const [key, value] of Object.entries(propertiesMap)) {
     descriptors[value as any] = {
       get() {
-        return unrefOrGet(obj)?.[key];
+        return (unrefOrGet(obj) as any)?.[key];
       },
     };
   }
