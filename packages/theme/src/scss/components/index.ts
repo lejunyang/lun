9f6ca708fa793105ac0b8e6a89a2f41261f48271
lejunyang <lejunyang@qq.com>
2023-10-30 15:02:49 +0800

    feat(): add basic styles from spin to make it align with text

--- packages/theme/src/scss/components/index.ts
+++ packages/theme/src/scss/components/index.ts
@@ -1,7 +1,6 @@
 import { importInputSurfaceTheme } from './input/index.ts';
 
 export * from './input/index.ts';
-export * from './spin/index.ts';
 
 export function importSurfaceTheme() {
   importInputSurfaceTheme()
--- packages/theme/src/scss/components/spin/index.ts
+++
@@ -1,4 +0,0 @@
-import { createImportStyle } from '@lun/components';
-import style from './index.scss?inline';
-
-export const importSpinStyle = createImportStyle('spin', style);
