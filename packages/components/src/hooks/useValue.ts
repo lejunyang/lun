2b695f06e68bee2751e2afaafa74aa130d8391ac
lejunyang <lejunyang@qq.com>
2023-10-31 14:12:22 +0800

    feat(core/createUseModel): add handleDefaultEmit for createUseModel, add useOpenModel hook

--- packages/components/src/hooks/useValue.ts
+++ packages/components/src/hooks/useValue.ts
@@ -4,6 +4,23 @@ import { useShadowDom } from './shadowDom';
 
 export const useValueModel = createUseModel({ defaultKey: 'value', defaultEvent: 'update' }) as UseModel<'value'>;
 
+export const useOpenModel = createUseModel({
+  defaultKey: 'open',
+  defaultEvent: 'update',
+  handleDefaultEmit(emit) {
+    return (name: string, ...args: any[]) => {
+      emit(name, ...args);
+      if (name === 'update') {
+        if (args[0]) {
+          emit('open');
+        } else {
+          emit('close');
+        }
+      }
+    };
+  },
+}) as UseModel<'open'>;
+
 /**
  * used to make CE compatible with v-model.\
  * If we want to make v-model natively work on CE, we need to do a lot of work, such as delegate events and sync the value.
