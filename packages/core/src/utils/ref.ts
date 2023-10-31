f4ef56856b801cb37266612bcf66a410ddf4db8b
lejunyang <lejunyang@qq.com>
2023-10-31 14:10:50 +0800

    feat(core/utils): add common mergeRef

--- packages/core/src/utils/ref.ts
+++ packages/core/src/utils/ref.ts
@@ -20,3 +20,13 @@ export type ToRefLike<T extends {}> = { [k in keyof T]: MaybeRefLikeOrGetter<T[k
 export type ToMaybeRefLike<T extends {}, EX extends keyof T = never, M extends keyof T = Exclude<keyof T, EX>> = {
   [k in M]: T[k] | MaybeRefLikeOrGetter<T[k]>;
 } & { [k in EX]: T[k] };
+
+export function mergeRef(...args: ({ value: any } | { current: any } | ((r: any) => void))[]) {
+  return (r: any) => {
+    args.forEach((arg) => {
+      if (isFunction(arg)) arg(r);
+      else if ('value' in arg) arg.value = r;
+      else if ('current' in arg) arg.current = r;
+    });
+  };
+}
