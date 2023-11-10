763496a7eb5d066e22fe99eb84ebba729ce22563
lejunyang <lejunyang@qq.com>
2023-11-10 10:47:50 +0800

    feat(components): add some other math methods

--- packages/components/src/common/math.ts
+++ packages/components/src/common/math.ts
@@ -1,11 +1,15 @@
 export type MathMethods<T = number> = {
   isNumber(target: unknown): target is T;
   isNaN(target: unknown): boolean;
+  isZero(target: unknown): boolean;
   toNumber(target: unknown): T;
   plus(target: T, delta: T): T;
   minus(target: T, delta: T): T;
   max(...args: T[]): T;
   min(...args: T[]): T;
+  equals(target: T, other: T): boolean;
+  greaterThan(target: T, other: T): boolean;
+  lessThan(target: T, other: T): boolean;
 };
 
 export function createMath<T = number>(methods: MathMethods<T>) {
