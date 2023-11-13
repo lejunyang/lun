e7349ddbb6abc88efc13d9613307b8be9f3de7d5
lejunyang <lejunyang@qq.com>
2023-11-13 16:58:49 +0800

    feat(core): useOverflowWatcher

---
+++ packages/core/src/hooks/useOverflowWatcher.ts
@@ -0,0 +1,57 @@
+import { ref, watchEffect } from 'vue';
+import { isSupportResizeObserver, isOverflow } from '@lun/utils';
+import { tryOnScopeDispose } from './lifecycle';
+import { MaybeRefLikeOrGetter, unrefOrGet } from '../utils';
+
+export type UseOverflowWatcherOptions = {
+  disable?: MaybeRefLikeOrGetter<boolean>;
+  elGetter: () => HTMLElement | undefined | null;
+  onOverflowChange?: (isOverflow: boolean) => void;
+  box?: ResizeObserverBoxOptions;
+};
+
+export function useOverflowWatcher(options: UseOverflowWatcherOptions) {
+  const { elGetter, onOverflowChange, box, disable } = options;
+  const overflow = ref(false);
+
+  let observer: ResizeObserver | null = null;
+
+  const updateOverflow = () => {
+    const el = elGetter();
+    if (!el) return;
+    const temp = isOverflow(el);
+    if (temp !== overflow.value) {
+      overflow.value = temp;
+      onOverflowChange && onOverflowChange(temp);
+    }
+  };
+
+  const clean = () => {
+    if (observer) {
+      observer.disconnect();
+      observer = null;
+    }
+  };
+
+  const stopWatch = watchEffect(() => {
+    clean();
+    const element = elGetter();
+    const disabled = unrefOrGet(disable);
+    if (element && isSupportResizeObserver() && !disabled) {
+      observer = new ResizeObserver(updateOverflow);
+      observer.observe(element, { box });
+    }
+  });
+
+  const stop = () => {
+    stopWatch();
+    clean();
+  };
+
+  tryOnScopeDispose(stop);
+
+  return {
+    isOverflow: overflow,
+    stop,
+  };
+}
