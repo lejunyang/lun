e2e6a8573395e801f1680e23a93c60cee568ba2b
lejunyang <lejunyang@qq.com>
2023-11-13 17:00:19 +0800

    feat(components/tooltip): add overflow logic for tooltip

--- packages/components/src/components/tooltip/Tooltip.tsx
+++ packages/components/src/components/tooltip/Tooltip.tsx
@@ -2,19 +2,36 @@ import { defineSSRCustomElement } from 'custom';
 import { tooltipProps } from './type';
 import { createDefineElement, renderElement } from 'utils';
 import { definePopover } from '../popover/Popover';
+import { useOverflowWatcher } from '@lun/core';
+import { useShadowDom } from 'hooks';
+import { isFunction } from '@lun/utils';
 
 const name = 'tooltip';
 const Tooltip = defineSSRCustomElement({
   name,
   props: tooltipProps,
   setup(props) {
+    const shadow = useShadowDom();
+    const { isOverflow } = useOverflowWatcher({
+      disable: () => !['enable', 'open'].includes(props.overflow!),
+      elGetter: () => shadow.CE,
+    });
     return () => {
+      const { open, overflow, beforeOpen } = props;
+      const overflowOpen = overflow === 'open' && isOverflow.value ? true : undefined;
       return renderElement(
         'popover',
-        props,
+        {
+          ...props,
+          open: open !== undefined ? open : overflowOpen,
+          beforeOpen() {
+            if (isFunction(beforeOpen) && beforeOpen() === false) return false;
+            if (overflow === 'enable' && !isOverflow.value) return false;
+          },
+        },
         <>
           <slot></slot>
-          <slot name="content" slot="pop-content"></slot>
+          <slot name="tooltip" slot="pop-content"></slot>
         </>
       );
     };
