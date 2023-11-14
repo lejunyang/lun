aa16569697f3516a2eb97bb2ff9f3e02503cd687
lejunyang <lejunyang@qq.com>
2023-11-14 09:59:54 +0800

    fix(components/tooltip): fix overflow issue caused by text, optimize style

--- packages/components/src/components/tooltip/Tooltip.tsx
+++ packages/components/src/components/tooltip/Tooltip.tsx
@@ -4,33 +4,42 @@ import { createDefineElement, renderElement } from 'utils';
 import { definePopover } from '../popover/Popover';
 import { useOverflowWatcher } from '@lun/core';
 import { useShadowDom } from 'hooks';
-import { isFunction } from '@lun/utils';
+import { getInnerTextOfSlot, isFunction } from '@lun/utils';
+import { ref } from 'vue';
 
 const name = 'tooltip';
 const Tooltip = defineSSRCustomElement({
   name,
   props: tooltipProps,
-  setup(props) {
+  inheritAttrs: false,
+  setup(props, { attrs }) {
     const shadow = useShadowDom();
+    const slotRef = ref<HTMLSlotElement>();
     const { isOverflow } = useOverflowWatcher({
       disable: () => !['enable', 'open'].includes(props.overflow!),
       elGetter: () => shadow.CE,
+      getText: () => getInnerTextOfSlot(slotRef.value),
     });
     return () => {
-      const { open, overflow, beforeOpen } = props;
+      const { open, overflow, beforeOpen, target } = props;
       const overflowOpen = overflow === 'open' && isOverflow.value ? true : undefined;
       return renderElement(
         'popover',
         {
+          ...attrs,
           ...props,
           open: open !== undefined ? open : overflowOpen,
           beforeOpen() {
             if (isFunction(beforeOpen) && beforeOpen() === false) return false;
             if (overflow === 'enable' && !isOverflow.value) return false;
           },
+          // make popover display: contents and set target to tooltip CE rather than popover CE
+          // so that overflow style can work on tooltip CE
+          target: target !== undefined ? target : shadow.CE,
+          style: `${attrs.style || ''}display: contents`,
         },
         <>
-          <slot></slot>
+          <slot ref={slotRef}></slot>
           <slot name="tooltip" slot="pop-content"></slot>
         </>
       );
