96131694d6ffb64c2e0d1f3728a30f84353daeb4
lejunyang <lejunyang@qq.com>
2023-11-16 17:56:22 +0800

    feat(components/useValue): add useCheckedModel for switch and checkbox

--- packages/components/src/hooks/useValue.ts
+++ packages/components/src/hooks/useValue.ts
@@ -4,26 +4,57 @@ import { useShadowDom } from './shadowDom';
 import { FormInputCollector } from '../components/form-item';
 import { getCurrentInstance } from 'vue';
 
+const extra = () => {
+  const vm = getCurrentInstance();
+  const context = FormInputCollector.child();
+  if (!context || !vm) return;
+  return [context, vm] as const;
+};
+
 export const useValueModel = createUseModel({
   defaultKey: 'value',
   defaultEvent: 'update',
-  extra() {
-    const vm = getCurrentInstance();
-    const context = FormInputCollector.child();
-    if (!context) return;
-    return [context, vm] as const;
-  },
+  extra,
   getFromExtra(extra) {
-    if (!extra) return;
     const [context, vm] = extra;
     return context.getValue(vm);
   },
   setByExtra(extra, value) {
-    if (!extra) return;
     const [context, vm] = extra;
     context.setValue(vm, value);
   },
 }) as UseModel<'value'>;
+
+/** used for switch, checkbox, which have 'checked', 'trueValue' and 'falseValue' props. */
+export const useCheckedModel = createUseModel({
+  defaultKey: 'checked',
+  defaultEvent: 'update',
+  extra,
+  getFromExtra(extra) {
+    const [context, vm] = extra;
+    const value = context.getValue(vm);
+    const { trueValue, falseValue } = vm.props;
+    if (trueValue !== undefined) return value === trueValue;
+    if (falseValue !== undefined) return value !== falseValue;
+  },
+  setByExtra(extra, value) {
+    const [context, vm] = extra;
+    const { trueValue, falseValue } = vm.props;
+    context.setValue(vm, value ? trueValue : falseValue);
+  },
+  handleDefaultEmit(emit, vm) {
+    return (name: string, ...args: any[]) => {
+      const { trueValue, falseValue } = vm.props;
+      if (name === 'update') {
+        const checked = args[0];
+        emit('update', {
+          value: checked ? trueValue : falseValue,
+          checked,
+        });
+      } else emit(name, ...args);
+    };
+  },
+}) as UseModel<'checked'>;
 
 export const useOpenModel = createUseModel({
   defaultKey: 'open',
