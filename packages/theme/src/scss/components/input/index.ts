a24c6835b95af2d3fb92cb0d444571e04cfaa0cb
lejunyang <lejunyang@qq.com>
2023-11-17 14:53:51 +0800

    feat(theme/input): extract basic style, add size-2 size-3 style, fix background style

--- packages/theme/src/scss/components/input/index.ts
+++ packages/theme/src/scss/components/input/index.ts
@@ -1,7 +1,7 @@
 import { createImportStyle } from '@lun/components';
-import basic from './index.scss?inline';
+import basic from './basic.scss?inline';
 import surface from './surface.scss?inline';
 
-export const importInputStyle = createImportStyle('input', basic);
+export const importInputBasicTheme = createImportStyle('input', basic);
 
 export const importInputSurfaceTheme = createImportStyle('input', surface);
