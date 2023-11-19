9089f57ea012394008e3b1bcbc0bbd457778403b
lejunyang <lejunyang@qq.com>
2023-11-19 23:19:11 +0800

    feat(theme/tag): add tag remove transition

--- packages/theme/src/scss/components/index.ts
+++ packages/theme/src/scss/components/index.ts
@@ -1,6 +1,7 @@
 import { importButtonBasicTheme, importButtonSurfaceTheme } from './button/index.ts';
 import { importInputBasicTheme, importInputSurfaceTheme } from './input/index.ts';
 import { importSwitchBasicTheme } from './switch/index.ts';
+import { importTagBasicTheme, importTagSurfaceTheme } from './tag/index.ts';
 
 export * from './input/index.ts';
 
@@ -8,9 +9,11 @@ export function importBasicTheme() {
   importButtonBasicTheme();
   importInputBasicTheme();
   importSwitchBasicTheme();
+  importTagBasicTheme();
 }
 
 export function importSurfaceTheme() {
   importButtonSurfaceTheme();
   importInputSurfaceTheme();
+  importTagSurfaceTheme();
 }
