f1a3577071e12ce8a28ac5a67b310754171a13e0
lejunyang <lejunyang@qq.com>
2023-11-20 17:01:45 +0800

    feat(theme/tag): add solid theme style, fix surface theme style

--- packages/theme/src/scss/components/tag/index.ts
+++ packages/theme/src/scss/components/tag/index.ts
@@ -1,6 +1,12 @@
 import { createImportStyle } from '@lun/components';
 import basic from './basic.scss?inline';
+import outline from './outline.scss?inline';
+import soft from './soft.scss?inline';
+import solid from './solid.scss?inline';
 import surface from './surface.scss?inline';
 
 export const importTagBasicTheme = createImportStyle('tag', basic);
+export const importTagOutlineTheme = createImportStyle('tag', outline);
+export const importTagSoftTheme = createImportStyle('tag', soft);
+export const importTagSolidTheme = createImportStyle('tag', solid);
 export const importTagSurfaceTheme = createImportStyle('tag', surface)
