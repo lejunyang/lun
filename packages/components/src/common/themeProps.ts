403f74131c53c331d8aae205a78421b084f927cc
lejunyang <lejunyang@qq.com>
2023-11-21 10:05:43 +0800

    feat(theme): add theme color style for components with color prop

--- packages/components/src/common/themeProps.ts
+++ packages/components/src/common/themeProps.ts
@@ -1,10 +1,39 @@
 import { Responsive } from '@lun/core';
 import { ExtractPropTypes, PropType } from 'vue';
 
+export const themeColors = Object.freeze([
+  'gray',
+  'gold',
+  'bronze',
+  'brown',
+  'yellow',
+  'amber',
+  'orange',
+  'tomato',
+  'red',
+  'ruby',
+  'crimson',
+  'pink',
+  'plum',
+  'purple',
+  'violet',
+  'iris',
+  'indigo',
+  'blue',
+  'cyan',
+  'teal',
+  'jade',
+  'green',
+  'grass',
+  'lime',
+  'mint',
+  'sky',
+] as const);
+
 export const themeProps = {
   size: { type: [String, Object] as PropType<Responsive<'1' | '2' | '3'>> },
-  color: { type: String },
-  variant: { type: String as PropType<'solid' | 'soft' | 'surface' | 'outline' | string> },
+  color: { type: String as PropType<(typeof themeColors)[number]> },
+  variant: { type: String as PropType<'solid' | 'soft' | 'surface' | 'outline'> },
   radius: { type: String as PropType<'none' | 'small' | 'medium' | 'large' | 'full'> },
   highContrast: { type: Boolean, default: undefined },
 };
