fa608caa1352329e887219f88d2653afa4505bf3
lejunyang <lejunyang@qq.com>
2023-11-24 10:59:26 +0800

    feat(theme/button): add solid theme styles for button, add highContrast style for surface theme

--- packages/theme/src/scss/components/button/index.ts
+++ packages/theme/src/scss/components/button/index.ts
@@ -1,6 +1,12 @@
 import { createImportStyle } from "@lun/components";
 import basic from './basic.scss?inline';
+import outline from './outline.scss?inline';
+import soft from './soft.scss?inline';
+import solid from './solid.scss?inline';
 import surface from './surface.scss?inline';
 
 export const importButtonBasicTheme = createImportStyle('button', basic);
+export const importButtonOutlineTheme = createImportStyle('button', outline);
+export const importButtonSoftTheme = createImportStyle('button', soft);
+export const importButtonSolidTheme = createImportStyle('button', solid);
 export const importButtonSurfaceTheme = createImportStyle('button', surface);
