e2ed5defe75bf4f690c1e8b3bf2e7311159a8812
lejunyang <lejunyang@qq.com>
2023-11-27 15:49:29 +0800

    feat: add some utils for running dynamic code on browser, support vue jsx for now

---
+++ src/utils/index.ts
@@ -0,0 +1,59 @@
+// @ts-ignore
+import url from 'esbuild-wasm/esbuild.wasm?url';
+import { transform, initialize } from 'esbuild-wasm';
+import * as vue from 'vue';
+import * as components from '@lun/components';
+import * as core from '@lun/core';
+import * as theme from '@lun/theme';
+import * as utils from '@lun/utils';
+import * as react from 'react';
+
+const allowedImport = new Set(['vue', 'react', '@lun/components', '@lun/core', '@lun/theme', '@lun/utils']);
+const dependencies = {
+  vue,
+  react,
+  '@lun/components': components,
+  '@lun/core': core,
+  '@lun/theme': theme,
+  '@lun/utils': utils,
+};
+
+const initResult = (async () => {
+  return initialize({
+    wasmURL: url,
+  });
+})();
+
+export async function buildDepRequire(code: string) {
+  const names: string[] = [];
+  const regexp = /require\(['"](.*)['"]\)/g;
+  let match = regexp.exec(code);
+  while (match) {
+    names.push(match[1]);
+    match = regexp.exec(code);
+  }
+  return (name: string) => {
+    if (!allowedImport.has(name)) {
+      throw new Error(`import "${name}" is not allowed, can be only one of ${Array.from(allowedImport).join(', ')}`);
+    }
+    return dependencies[name];
+  };
+}
+
+export async function runVueJSXCode(code: string) {
+  await initResult;
+  const res = await transform(code, {
+    loader: 'tsx',
+    format: 'iife',
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    jsxImportSource: 'vue',
+    globalName: 'result', // `var result = (() => {})();`
+  });
+  const requireDep = await buildDepRequire(res.code);
+  if (!res.code.includes('var result')) throw new Error('Must export a default component');
+  const func = new Function('require', 'h', res.code + ';return result;');
+  const result = func(requireDep, vue.h);
+  if (!utils.isFunction(result?.default)) throw new Error('Must export a default component');
+  return result.default();
+}
