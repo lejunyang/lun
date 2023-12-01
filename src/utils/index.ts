ea64cdc2d17fc717b79c5a980c587ef952cc074e
lejunyang <lejunyang@qq.com>
2023-12-01 15:41:08 +0800

    perf(docs): lazy import all the related dependencies for dynamic run

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,29 +1,52 @@
-// @ts-ignore
 import url from 'esbuild-wasm/esbuild.wasm?url';
-import { transform, initialize } from 'esbuild-wasm';
-import * as vue from 'vue';
-import * as components from '@lun/components';
-import * as core from '@lun/core';
-import * as theme from '@lun/theme';
-import * as utils from '@lun/utils';
-import * as react from 'react';
+import { isClient, isFunction } from '@lun/utils';
 
-const allowedImport = new Set(['vue', 'react', '@lun/components', '@lun/core', '@lun/theme', '@lun/utils']);
+const allowedImport = new Set([
+  'vue',
+  'react',
+  'react-dom',
+  'react-dom/client',
+  '@lun/components',
+  '@lun/core',
+  '@lun/theme',
+  '@lun/utils',
+]);
+
 const dependencies = {
-  vue,
-  react,
-  '@lun/components': components,
-  '@lun/core': core,
-  '@lun/theme': theme,
-  '@lun/utils': utils,
+  vue: () => import('vue'),
+  react: () => import('react'),
+  'react-dom': () => import('react-dom'),
+  'react-dom/client': () => import('react-dom/client'),
+  '@lun/components': () => import('@lun/components'),
+  '@lun/core': () => import('@lun/core'),
+  '@lun/theme': () => import('@lun/theme'),
+  '@lun/utils': () => import('@lun/utils'),
+} as any as {
+  vue: typeof import('vue');
+  react: typeof import('react');
+  'react-dom': typeof import('react-dom');
+  'react-dom/client': typeof import('react-dom/client');
+  '@lun/components': typeof import('@lun/components');
+  '@lun/core': typeof import('@lun/core');
+  '@lun/theme': typeof import('@lun/theme');
+  '@lun/utils': typeof import('@lun/utils');
 };
 
-const initResult = (async () => {
-  if (!utils.isClient()) {
-    return;
-  }
-  return initialize({
-    wasmURL: url,
+const initPromise = (async () => {
+  if (!isClient()) return;
+  // don't load esbuild until window is loaded
+  return new Promise<void>((resolve) => {
+    const load = async () => {
+      const { initialize } = await import('esbuild-wasm');
+      await initialize({
+        wasmURL: url,
+      });
+      window.removeEventListener('load', load);
+      if (isFunction(dependencies.vue)) dependencies.vue = await dependencies.vue();
+      import('../components/Editor.vue'); // trigger load Editor but don't wait it
+      resolve();
+    };
+    window.addEventListener('load', load);
   });
 })();
 
@@ -35,16 +58,22 @@ export async function buildDepRequire(code: string) {
     names.push(match[1]);
     match = regexp.exec(code);
   }
-  return (name: keyof typeof dependencies) => {
+  for (const name of names) {
     if (!allowedImport.has(name)) {
       throw new Error(`import "${name}" is not allowed, can be only one of ${Array.from(allowedImport).join(', ')}`);
     }
+    if (isFunction(dependencies[name])) {
+      dependencies[name] = await dependencies[name]();
+    }
+  }
+  return (name: keyof typeof dependencies) => {
     return dependencies[name];
   };
 }
 
 export async function runVueJSXCode(code: string) {
-  await initResult;
+  await initPromise;
+  const { transform } = await import('esbuild-wasm');
   const errorMsg = 'Must export a default vnode or a default function that returns a vnode';
   const res = await transform(code, {
     loader: 'tsx',
@@ -57,7 +86,7 @@ export async function runVueJSXCode(code: string) {
   const requireDep = await buildDepRequire(res.code);
   if (!res.code.includes('var result')) throw new Error(errorMsg);
   const func = new Function('require', 'h', 'Fragment', res.code + ';return result;');
-  const result = func(requireDep, vue.h, vue.Fragment);
+  const result = func(requireDep, dependencies.vue.h, dependencies.vue.Fragment);
   if (!result?.default) throw new Error(errorMsg);
   return result.default;
 }
