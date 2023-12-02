4ae896db212a70c6cbe38ce8a72eb32930bd51fb
lejunyang <lejunyang@qq.com>
2023-12-02 23:24:25 +0800

    docs: radio

---
+++ src/docs/components/radio/differentSizes.vue.tsx
@@ -0,0 +1,9 @@
+export default function () {
+  return (
+    <>
+      <l-radio size="1" checked>Size 1</l-radio>
+      <l-radio size="2" checked>Size 2</l-radio>
+      <l-radio size="3" checked>Size 3</l-radio>
+    </>
+  );
+}
