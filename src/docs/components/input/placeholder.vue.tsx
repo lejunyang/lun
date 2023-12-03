35b35245d8f168cc0d859f16360befb2f00afb95
lejunyang <lejunyang@qq.com>
2023-12-03 18:10:01 +0800

    docs: input

---
+++ src/docs/components/input/placeholder.vue.tsx
@@ -0,0 +1,9 @@
+export default function () {
+  return (
+    <>
+      <l-input placeholder="placeholder" />
+      <l-input placeholder="disabled" disabled />
+      <l-input placeholder="readonly" readonly />
+    </>
+  );
+}
---
+++ src/docs/components/input/showLengthInfo.vue.tsx
@@ -0,0 +1,3 @@
+export default function () {
+  return <l-input showLengthInfo maxLength="10" />;
+}
