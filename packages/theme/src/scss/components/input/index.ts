480d172e6b020c044a8439ae2b8bb5eaad8c4f28
lejunyang <lejunyang@qq.com>
2023-12-03 17:10:40 +0800

    feat(theme/input): add soft theme styles, add surface disabled styles

--- packages/theme/src/scss/components/input/index.ts
+++ packages/theme/src/scss/components/input/index.ts
@@ -1,7 +1,8 @@
 import { createImportStyle } from '@lun/components';
 import basic from './basic.scss?inline';
 import surface from './surface.scss?inline';
+import soft from './soft.scss?inline';
 
 export const importInputBasicTheme = createImportStyle('input', basic);
-
+export const importInputSoftTheme = createImportStyle('input', soft);
 export const importInputSurfaceTheme = createImportStyle('input', surface);
