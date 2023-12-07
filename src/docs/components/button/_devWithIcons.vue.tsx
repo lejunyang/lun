c991a8e0aa0f2b0cbb53b4dc89ce7d5b0fbee931
lejunyang <lejunyang@qq.com>
2023-12-07 18:10:23 +0800

    docs: button

--- src/docs/components/button/_devWithIcons.vue.tsx
+++ src/docs/components/button/_devWithIcons.vue.tsx
@@ -1,19 +1,27 @@
 export default function () {
   return (
     <>
-      检查文字与按钮对齐x
-      <l-button variant="ghost">
-        <l-icon name="clear" />
-      </l-button>
-      <l-button variant="ghost">
-        文字x
-      </l-button>
-      <l-button>
-        <l-icon name="down" />
-      </l-button>
-      <l-button>
-        <l-icon name="x" />
-      </l-button>
+      <div class="container">
+        检查文字与按钮对齐x
+        <l-button variant="ghost">
+          <l-icon name="clear" />
+        </l-button>
+        <l-button variant="ghost">文字x</l-button>
+        <l-button>
+          <l-icon name="down" />
+        </l-button>
+        <l-button>
+          <l-icon name="x" />
+        </l-button>
+      </div>
+      <div class="container">
+        <l-button variant="surface" holdOn="1000">surface</l-button>
+        <l-button variant="solid" holdOn="1000">solid</l-button>
+        <l-button variant="solid" holdOn="1000" highContrast>solid highContrast</l-button>
+        <l-button variant="outline" holdOn="1000">outline</l-button>
+        <l-button variant="soft" holdOn="1000">soft</l-button>
+        <l-button variant="ghost" holdOn="1000">ghost</l-button>
+      </div>
     </>
   );
 }
