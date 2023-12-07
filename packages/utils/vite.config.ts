a99abadcebf8fd4a61a02a4eef8ecadd439bdae0
lejunyang <lejunyang@qq.com>
2023-12-07 15:25:12 +0800

    chore: use getViteConfig, change utils package build command to generate development build and production build

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -1,26 +1,2 @@
-import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'node:url';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	define: {
-		// __DEV__: "!!(process.env.NODE_ENV !== 'production')",
-		__DEV__: 'true',
-  },
-  build: {
-		lib: {
-			entry: './index.ts',
-			name: '@lun/utils',
-			fileName: 'lun-utils',
-		},
-		minify: false,
-		rollupOptions: {
-			external: ['vue', /@lun\/.+/],
-		}
-	},
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-});
+import { getViteConfig } from '../../utils/getViteConfig';
+export default getViteConfig('@lun/utils');
