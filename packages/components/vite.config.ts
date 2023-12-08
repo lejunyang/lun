b8d5bf301a1525d232351e5a9a10f1dc36d48e73
lejunyang <lejunyang@qq.com>
2023-12-08 11:52:21 +0800

    chore: unify vite.config.ts, change build commands and export path

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,56 +1,27 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		vue({
-			template: {
-				compilerOptions: {
-					isCustomElement: (tag) => tag.startsWith('l-'),
-				},
-			},
-		}),
-		vueJsx(),
-	],
-	optimizeDeps: {
-		exclude: ['vue'],
-	},
-	build: {
-		lib: {
-			entry: './index.ts',
-			name: '@lun/components',
-			fileName: 'lun-components',
-		},
-		minify: false,
-		rollupOptions: {
-			external: ['vue', /@lun\/.+/],
-		}
-	},
-	define: {
-		// __DEV__: "!!(process.env.NODE_ENV !== 'production')",
-		__DEV__: 'true',
-	},
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
-			common: fileURLToPath(new URL('./src/common', import.meta.url)),
-			config: fileURLToPath(new URL('./src/components/config', import.meta.url)),
-			custom: fileURLToPath(new URL('./src/custom', import.meta.url)),
-			utils: fileURLToPath(new URL('./src/utils', import.meta.url)),
-			hooks: fileURLToPath(new URL('./src/hooks', import.meta.url)),
-		},
-	},
-	css: {
-		// css预处理器
-		preprocessorOptions: {
-			scss: {
-				// 引入 mixin.scss 这样就可以在全局中使用 mixin.scss中预定义的变量了
-				// 给导入的路径最后加上 ;
-				// additionalData: '@import "@/assets/style/mixin.scss";',
-			},
-		},
-	},
+import { getViteConfig } from '../../utils/getViteConfig';
+
+export default getViteConfig('@lun/components', {
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith('l-'),
+        },
+      },
+    }),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      common: fileURLToPath(new URL('./src/common', import.meta.url)),
+      config: fileURLToPath(new URL('./src/components/config', import.meta.url)),
+      custom: fileURLToPath(new URL('./src/custom', import.meta.url)),
+      utils: fileURLToPath(new URL('./src/utils', import.meta.url)),
+      hooks: fileURLToPath(new URL('./src/hooks', import.meta.url)),
+    },
+  },
 });
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,31 +1,3 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
+import { getViteConfig } from '../../utils/getViteConfig';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [vue()],
-	optimizeDeps: {
-		exclude: ['vue'],
-	},
-	define: {
-		// __DEV__: "!!(process.env.NODE_ENV !== 'production')",
-		__DEV__: 'true',
-	},
-	build: {
-		lib: {
-			entry: './index.ts',
-			name: '@lun/core',
-			fileName: 'lun-core',
-		},
-		minify: false,
-		rollupOptions: {
-			external: ['vue', /@lun\/.+/],
-		}
-	},
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-});
+export default getViteConfig('@lun/core');
