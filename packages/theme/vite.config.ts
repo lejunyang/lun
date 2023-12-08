ff9213e0b0ba858487d85fb6680f98fc57169e67
lejunyang <lejunyang@qq.com>
2023-12-08 11:52:21 +0800

    chore: unify vite.config.ts, change build commands and export path

--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -1,40 +1,10 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
 import postcssLogical from 'postcss-logical';
+import { getViteConfig } from '../../utils/getViteConfig';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  optimizeDeps: {
-    exclude: ['vue'],
-  },
-  define: {
-    // __DEV__: "!!(process.env.NODE_ENV !== 'production')",
-    __DEV__: 'true',
-  },
-  build: {
-    lib: {
-      entry: './index.ts',
-      name: '@lun/theme',
-      fileName: 'lun-theme',
-    },
-    minify: false,
-    rollupOptions: {
-      external: ['vue', /@lun\/.+/],
-    },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+export default getViteConfig('@lun/theme', {
   css: {
     postcss: {
       plugins: [postcssLogical()],
-    },
-    preprocessorOptions: {
-      scss: {},
     },
   },
 });
