138388ff85c69f2514c91c9744a4168b4748c594
lejunyang <lejunyang@qq.com>
2023-12-08 16:57:47 +0800

    chore: add declare types file generate script

---
+++ packages/components/generateModuleTypes.js
@@ -0,0 +1,53 @@
+import { components } from '@lun/components';
+import { camelize, capitalize } from 'vue';
+import fs from 'fs';
+
+const vueCompTypes = [];
+const vueJSXTypes = [];
+const htmlTypes = [];
+const reactTypes = [];
+
+components.forEach((componentTag) => {
+  const componentCamelName = camelize(capitalize(componentTag));
+  const compType = `import('./index').${componentCamelName}`;
+  const propType = `import('./index').${componentCamelName}Props`;
+  vueCompTypes.push(`    L${componentCamelName}: ${compType};`);
+  vueJSXTypes.push(`    'l-${componentTag}': ${propType};`);
+  htmlTypes.push(`    'l-${componentTag}': ${compType};`);
+  reactTypes.push(`      'l-${componentTag}': ${propType};`);
+});
+
+fs.writeFileSync(
+  './dist/vue-elements.d.ts',
+  `declare module 'vue' {
+  interface GlobalComponents {
+${vueCompTypes.join('\n')}
+  }
+  interface IntrinsicElementAttributes {
+${vueJSXTypes.join('\n')}
+  }
+}`,
+  { encoding: 'utf8' }
+);
+
+fs.writeFileSync(
+  './dist/html-elements.d.ts',
+  `declare global {
+  interface HTMLElementTagNameMap {
+${htmlTypes.join('\n')}
+  }
+}`,
+  { encoding: 'utf8' }
+);
+
+fs.writeFileSync(
+  './dist/react-elements.d.ts',
+  `declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+${reactTypes.join('\n')}
+    }
+  }
+}`,
+  { encoding: 'utf8' }
+);
