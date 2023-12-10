2c73852befb418111a98ee11df31d8fa65a98307
lejunyang <lejunyang@qq.com>
2023-12-10 23:42:19 +0800

    feat(components): add common prop constructor

---
+++ packages/components/src/common/propConstructor.ts
@@ -0,0 +1,26 @@
+import { AnyFn } from "@lun/utils";
+import { PropType } from "vue";
+
+export function PropString<T extends string>() {
+  return { type: String as unknown as PropType<T> };
+}
+
+export function PropNumber<T extends number>() {
+  return { type: Number as unknown as PropType<T> };
+}
+
+export function PropBoolean<T extends boolean>() {
+  return { type: Boolean as unknown as PropType<T> };
+}
+
+export function PropObject<T extends {}>() {
+  return { type: Object as unknown as PropType<T> };
+}
+
+export function PropArray<T extends any[]>() {
+  return { type: Array as unknown as PropType<T> };
+}
+
+export function PropFunction<T extends AnyFn>() {
+  return { type: Function as unknown as PropType<T> };
+}
