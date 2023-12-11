450fd5a975dffd26076ab2cb690eae30fdeb7a22
lejunyang <lejunyang@qq.com>
2023-12-11 19:17:09 +0800

    feat(theme): basic styles for gird layout form

---
+++ packages/theme/src/scss/components/form/index.ts
@@ -0,0 +1,4 @@
+import { createImportStyle } from '@lun/components';
+import basic from './basic.scss?inline';
+
+export const importFormBasicTheme = createImportStyle('form', basic);
--- packages/theme/src/scss/components/index.ts
+++ packages/theme/src/scss/components/index.ts
@@ -1,6 +1,8 @@
 import { importButtonBasicTheme, importButtonOutlineTheme, importButtonSoftTheme, importButtonSolidTheme, importButtonSurfaceTheme } from './button/index.ts';
 import { importCheckboxBasicTheme, importCheckboxSoftTheme, importCheckboxSurfaceTheme } from './checkbox/index.ts';
 import { importDialogBasicTheme } from './dialog/index.ts';
+import { importFormBasicTheme } from './form/index.ts';
+import { importFormItemBasicTheme } from './form-item/index.ts';
 import { importInputBasicTheme, importInputSoftTheme, importInputSurfaceTheme } from './input/index.ts';
 import { importRadioBasicTheme } from './radio/index.ts';
 import { importSwitchBasicTheme, importSwitchSurfaceTheme } from './switch/index.ts';
@@ -18,6 +20,8 @@ export function importBasicTheme() {
   importButtonBasicTheme();
   importCheckboxBasicTheme();
   importDialogBasicTheme();
+  importFormBasicTheme();
+  importFormItemBasicTheme();
   importInputBasicTheme();
   importRadioBasicTheme();
   importSwitchBasicTheme();
