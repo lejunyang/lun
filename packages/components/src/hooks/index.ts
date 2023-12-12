afc9ccf3e5ef27b9ef296408f36801b23dbc7116
lejunyang <lejunyang@qq.com>
2023-12-12 11:19:57 +0800

    perf(components/select): use 'useOptions' and delete some common codes

--- packages/components/src/hooks/index.ts
+++ packages/components/src/hooks/index.ts
@@ -1,6 +1,7 @@
 export * from './shadowDom';
 export * from './useDelegateCEEvents';
 export * from './useNameSpace';
+export * from './useOptions';
 export * from './useSetupEvent';
 export * from './useStyles';
 export * from './useValue';
--- packages/components/src/hooks/useOptions.ts
+++ packages/components/src/hooks/useOptions.ts
@@ -16,10 +16,15 @@ export type CommonOptions<HasChildren extends boolean = false> = HasChildren ext
   ? (CommonOptionGroup | CommonOption)[]
   : CommonOption[];
 
-type OptionNameMap = { label?: string; value?: string; children?: string };
+export type CommonOptionNameMap = { label?: string; value?: string; children?: string };
+
+/**
+ * @param _hasChildren if the option has children
+ * @returns props for element that has options(like select, radio-group, checkbox-group)
+ */
 export const createOptionProps = <
   T extends boolean = false,
-  M = T extends true ? OptionNameMap : Omit<OptionNameMap, 'children'>
+  M = T extends true ? CommonOptionNameMap : Omit<CommonOptionNameMap, 'children'>
 >(
   _hasChildren?: T
 ) => {
@@ -30,11 +35,18 @@ export const createOptionProps = <
   };
 };
 
+/**
+ * Custom hook for handling options in components like select, radio-group, and checkbox-group.
+ * @param props - The props object of setup function first parameter that contains options and optionNameMap.
+ * @param optionName - The name of the component that renders each option.
+ * @param groupOptionName - The name of the component that renders each option group.
+ * @returns An object containing the options and a render function.
+ */
 export function useOptions<
   G extends ComponentKey | undefined,
   HasChildren extends boolean = G extends undefined ? false : true
 >(
-  props: { options?: MaybePromiseOrGetter<CommonOptions<HasChildren>>; optionNameMap?: OptionNameMap },
+  props: { options?: MaybePromiseOrGetter<CommonOptions<HasChildren>>; optionNameMap?: CommonOptionNameMap },
   optionName: ComponentKey,
   groupOptionName?: G
 ) {
