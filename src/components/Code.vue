4e099acc8bafa67505e5b1c67648b89e7597519a
lejunyang <lejunyang@qq.com>
2023-12-23 23:11:16 +0800

    docs: add reset icon for code case

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -8,15 +8,13 @@
       <VCustomRenderer v-bind="rendererProps"></VCustomRenderer>
     </div>
     <div class="code-block-actions">
-      <svg
-        :title="showEditor ? 'Hide code' : 'Show code'"
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        viewBox="0 0 16 16"
-        @click="showEditor = !showEditor"
-      >
+      <svg v-bind="commonSVGProps" @click="resetCodes">
+        <title>Refresh</title>
+        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
+        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+      </svg>
+      <svg v-bind="commonSVGProps" @click="showEditor = !showEditor">
+        <title>{{ showEditor ? 'Hide code' : 'Show code' }}</title>
         <path
           d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z"
         />
@@ -32,6 +30,14 @@ import { debounce, runIfFn } from '@lun/utils';
 import { VCustomRenderer } from '@lun/components';
 import { runVueTSXCode, runReactTSXCode } from '../utils';
 import { inBrowser } from 'vitepress';
+
+const commonSVGProps = {
+  xmlns: 'http://www.w3.org/2000/svg',
+  width: '16',
+  height: '16',
+  fill: 'currentColor',
+  viewBox: '0 0 16 16',
+};
 
 const Editor = inBrowser ? defineAsyncComponent(() => import('./Editor.vue')) : () => null;
 
@@ -63,6 +69,13 @@ const codesMap = reactive({
   html: props.html,
   react: props.react,
 } as Record<string, string>);
+
+const resetCodes = () => {
+  Object.keys(codesMap).forEach((key) => {
+    codesMap[key] = props[key];
+  });
+  rendererProps.key = Date.now();
+};
 
 const rendererProps = reactive({
   content: undefined as any,
@@ -150,6 +163,7 @@ main .code-container {
   padding: 5px;
   border: 1px solid var(--vp-c-divider);
   border-top: 1px dashed var(--vp-c-divider);
+  gap: 5px;
 
   svg {
     cursor: pointer;
