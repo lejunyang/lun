4f50ea0d8b5dae40478ac7e6b007d7ec1d30e062
lejunyang <lejunyang@qq.com>
2023-12-27 15:06:38 +0800

    refactor: use createPropFactory to create propConstructor

--- packages/utils/src/type/index.ts
+++ packages/utils/src/type/index.ts
@@ -2,5 +2,24 @@ export type Fn = () => void;
 export type AnyFn<R = any> = (...args: any[]) => R;
 export type CommonObject = Record<string | number | symbol, unknown>;
 export type AnyObject = Record<string | number | symbol, any>;
+export type Constructor<T = any> = new (...args: any[]) => T;
 
 export type EnsureParameters<T> = T extends (...args: infer P) => any ? P : never[];
+
+export type UnwrapPrimitive<T> = T extends String
+  ? string
+  : T extends Number
+  ? number
+  : T extends Boolean
+  ? boolean
+  : T extends BigInt
+  ? bigint
+  : T extends Symbol
+  ? symbol
+  : T extends Array<infer A> // Array and Function need to be placed before Object
+  ? A[]
+  : T extends Function
+  ? AnyFn
+  : T extends Object
+  ? object
+  : T;
