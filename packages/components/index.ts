d86c8b84ec359eae360a1d6337e44ad2e7da8c25
lejunyang <lejunyang@qq.com>
2023-12-27 15:04:10 +0800

    chore: add ReservedProps for generated vue types; remove useless files; remove export all from core and utils

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,8 +1,7 @@
-export * from '@lun/core';
-export * from '@lun/utils';
-
 export * from './src/components';
 export * from './src/custom';
 export * from './src/hooks';
 export * from './src/utils';
 export { themeColors } from './src/common';
+
+export { useForm } from '@lun/core';
