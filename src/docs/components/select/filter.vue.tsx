667f46197da716d6c59a3ba58d77fde4e41a1f01
lejunyang <lejunyang@qq.com>
2024-01-09 19:47:27 +0800

    docs: select filter

---
+++ src/docs/components/select/filter.vue.tsx
@@ -0,0 +1,29 @@
+import { optionsForSearch } from 'data';
+import { ref } from 'vue';
+
+const select = ref(null);
+const multi = ref(null);
+const input = ref(null);
+export default () => {
+  return (
+    <>
+      <l-select options={optionsForSearch} v-update={select.value} filter />
+      <l-select
+        options={optionsForSearch.map((i) => {
+          if (!input.value) return i;
+          return {
+            ...i,
+            content: i.label.split(input.value).join(`<span style="color: red;">${input.value}</span>`),
+            contentPreferHtml: true,
+          };
+        })}
+        multiple
+        v-update={multi.value}
+        filter
+        onInputUpdate={(e) => {
+          input.value = e.detail;
+        }}
+      />
+    </>
+  );
+};
