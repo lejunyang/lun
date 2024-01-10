72f043b62626c1ccc65bec4e86eee20e6bfe4162
lejunyang <lejunyang@qq.com>
2024-01-10 11:35:20 +0800

    feat(components/select): fix that active index will be reset after select changed or pop closed, add pointer leave for select-option, add enter down to toggle active option

--- packages/components/src/components/select/useActivateOption.ts
+++ packages/components/src/components/select/useActivateOption.ts
@@ -5,28 +5,33 @@ export function useActivateOption(
   collectorParent: CollectorParentReturn,
   params: MaybeRefLikeOrGetter<{ upDownToggle?: boolean; autoActivateFirst?: boolean }>,
 ) {
-  const activeIndex = ref();
+  const activeIndex = ref<number | undefined | null>();
   const isNotAllDisabled = computed(() => {
     return collectorParent.value.some((vm) => !vm.exposed?.disabled);
   });
   const firstSelectedIndex = computed(() => collectorParent.value.findIndex((vm) => vm.exposed?.selected));
+  const deactivate = () => (activeIndex.value = null);
   const methods = {
     isActive(vm: ComponentInternalInstance) {
       return activeIndex.value != null && activeIndex.value === collectorParent.getChildVmIndex(vm);
+    },
+    getActiveChild() {
+      return collectorParent.value[activeIndex.value!];
     },
     activate(vm: ComponentInternalInstance) {
       const index = collectorParent.getChildVmIndex(vm);
       if (index !== undefined) activeIndex.value = index;
     },
+    deactivate,
     isValidNow() {
-      const vmNow = collectorParent.value[activeIndex.value];
+      const vmNow = methods.getActiveChild();
       return vmNow && !vmNow.exposed?.disabled;
     },
     activateNext() {
       const { value } = activeIndex;
       const { length } = collectorParent.value;
       if (value == null || value === length - 1) activeIndex.value = 0;
-      else if (value < length - 1) activeIndex.value++;
+      else if (value < length - 1) activeIndex.value!++;
       // need to check if it is all disabled, or it may lead to a dead loop
       if (isNotAllDisabled.value && !methods.isValidNow()) methods.activateNext();
     },
@@ -34,20 +39,21 @@ export function useActivateOption(
       const { value } = activeIndex;
       const { length } = collectorParent.value;
       if (value == null || value === 0) activeIndex.value = length - 1;
-      else if (value > 0) activeIndex.value--;
+      else if (value > 0) activeIndex.value!--;
       if (isNotAllDisabled.value && !methods.isValidNow()) methods.activatePrev();
     },
-    resetIndex() {
+    initIndex() {
       if (!isNotAllDisabled.value) {
-        activeIndex.value = null;
+        activeIndex.value = undefined;
         return;
       }
+      if (activeIndex.value !== undefined) return;
       // if there is a selected option, activate it, or activate the first option according to the autoActivateFirst prop
       if (firstSelectedIndex.value !== -1) {
         activeIndex.value = firstSelectedIndex.value;
         return;
       }
-      if (unrefOrGet(params)?.autoActivateFirst && activeIndex.value == null && collectorParent.value.length) {
+      if (unrefOrGet(params)?.autoActivateFirst && collectorParent.value.length) {
         methods.activateNext();
       }
     },
@@ -64,6 +70,6 @@ export function useActivateOption(
       }
     },
   };
-  watchEffect(methods.resetIndex);
+  watchEffect(methods.initIndex);
   return { activeIndex, methods, handlers };
 }
