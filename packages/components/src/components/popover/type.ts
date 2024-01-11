abe1c557830e64ddab22c536054a152f9bcd548a
lejunyang <lejunyang@qq.com>
2024-01-11 17:14:26 +0800

    chore: popover comments

--- packages/components/src/components/popover/type.ts
+++ packages/components/src/components/popover/type.ts
@@ -1,23 +1,29 @@
 import {
   GetEventPropsFromEmits,
   PropBoolean,
+  PropFunction,
   PropNumber,
   PropObjOrFunc,
   PropObject,
+  PropStrOrArr,
   PropString,
   createTransitionProps,
   themeProps,
 } from 'common';
 import { Placement } from '@floating-ui/vue';
-import type { MaybeRefLikeOrGetter, PopoverTrigger, Responsive, VirtualElement } from '@lun/core';
-import type { CSSProperties, ExtractPropTypes, PropType, VNode } from 'vue';
+import type { MaybeRefLikeOrGetter, PopoverTrigger, VirtualElement } from '@lun/core';
+import type { CSSProperties, ExtractPropTypes, VNode } from 'vue';
 
 export const popoverProps = {
   ...createTransitionProps(),
 
+  /** used to manually set the open state of popover */
   open: PropBoolean(),
+  /** used to manually set the anchor target of popover */
   target: PropObject<MaybeRefLikeOrGetter<Element | VirtualElement>>(),
+  /** determine the implementation type of popover */
   type: PropString<'popover' | 'fixed' | 'teleport'>(),
+  /** determine the teleport target when 'type' is 'teleport', default to 'body' */
   to: PropString(),
 
   content: {},
@@ -29,24 +35,22 @@ export const popoverProps = {
   offset: PropNumber(),
   showArrow: PropBoolean(),
 
-  // make the pop content the same width or height as the target element.
+  /** used to make the pop content same width or height as the target element. */
   sync: PropString<'width' | 'height' | 'both'>(),
-  adjustPopStyle: {
-    type: Function as PropType<(result: CSSProperties, middlewareData: Record<string, any>) => CSSProperties | void>,
-  },
-  // whether use transform to position, useful when has animation conflict
+  /** used to dynamically set styles of pop element */
+  adjustPopStyle: PropFunction<(result: CSSProperties, middlewareData: Record<string, any>) => CSSProperties | void>(),
+  /** determine whether to use transform to position, useful when has animation conflict. */
   useTransform: PropBoolean(),
 
   openDelay: PropNumber(),
   closeDelay: PropNumber(),
-  triggers: { type: [String, Array] as PropType<PopoverTrigger | PopoverTrigger[]> },
+  triggers: PropStrOrArr<PopoverTrigger | PopoverTrigger[]>(),
   /** it's only for click trigger, used to toggle open and close state when click. */
   toggleMode: PropBoolean(),
-  beforeOpen: { type: Function as PropType<() => boolean | void> },
+  beforeOpen: PropFunction<() => boolean | void>(),
 
   ...themeProps,
   variant: PropString<'styleless' | string>(),
-  size: { type: [String, Object] as PropType<Responsive<'1' | '2' | '3'>> },
 };
 
 export const popoverEmits = {
