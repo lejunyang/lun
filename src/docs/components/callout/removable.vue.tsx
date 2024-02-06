6ced1259ed51b97fd1486db8f9019bb803e40457
lejunyang <lejunyang@qq.com>
2024-02-07 01:27:01 +0800

    docs: callout

--- src/docs/components/callout/removable.vue.tsx
+++
@@ -1,8 +0,0 @@
-import { sentence, text } from 'data';
-
-export default () => (
-  <>
-    <l-callout message={sentence} removable />
-    <l-callout message={sentence} description={text} removable />
-  </>
-);
