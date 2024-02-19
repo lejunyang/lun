c9cbf6c7a22f41163a889fe7c3fd277719431847
lejunyang <lejunyang@qq.com>
2024-02-19 15:53:58 +0800

    refactor(components/spin): move styles and animations from components package to theme package

--- packages/components/src/components/spin/Spin.tsx
+++ packages/components/src/components/spin/Spin.tsx
@@ -1,54 +1,22 @@
 import { defineSSRCustomElement } from 'custom';
-import { registryAnimation, useAnimation } from '../animation';
-import { onMounted } from 'vue';
-import { useComputedBreakpoints } from '@lun/core';
-import { createDefineElement, createImportStyle } from 'utils';
+import { createDefineElement } from 'utils';
 import { spinProps } from './type';
-import styles from './basic.scss?inline';
-
-registryAnimation('spin.rotate', {
-  keyframes: [{ transform: 'rotate(0deg)' }, { transform: 'rotate(1turn)' }],
-  options: {
-    duration: 1400,
-    easing: 'linear',
-    iterations: Infinity,
-  },
-});
-
-registryAnimation('spin.circle.stroke', {
-  keyframes: [
-    { strokeDasharray: '1px 200px', strokeDashoffset: 0 },
-    { offset: 0.3, strokeDasharray: '100px 200px', strokeDashoffset: '-15px' },
-    { strokeDasharray: '100px 200px', strokeDashoffset: '-120px' },
-  ],
-  options: {
-    duration: 1400,
-    easing: 'ease-in-out',
-    iterations: Infinity,
-  },
-});
+import { useNamespace } from 'hooks';
 
 const name = 'spin';
 export const Spin = defineSSRCustomElement({
   name,
   props: spinProps,
   setup(props) {
-    const [svgRef, svgAnimate] = useAnimation('spin.rotate');
-    const [circleRef, circleAnimate] = useAnimation('spin.circle.stroke');
-    const spinSizeClass = useComputedBreakpoints(() => props.size, 'l-spin-size');
+    const ns = useNamespace(name);
 
-    onMounted(() => {
-      svgAnimate();
-      circleAnimate();
-    });
     return () => {
       const { type } = props;
       switch (type) {
         case 'circle':
           return (
             <svg
-              class={spinSizeClass.value}
-              ref={svgRef}
+              class={[ns.t, ns.m(type)]}
               viewBox="0 0 50 50"
               width="1em"
               height="1em"
@@ -57,7 +25,6 @@ export const Spin = defineSSRCustomElement({
             >
               <circle
                 part="circle"
-                ref={circleRef}
                 cx="25"
                 cy="25"
                 r="20"
@@ -76,4 +43,3 @@ export const Spin = defineSSRCustomElement({
 export type tSpin = typeof Spin;
 
 export const defineSpin = createDefineElement(name, Spin);
-export const importSpinStyle = createImportStyle(name, styles);
