1894f7c3f24419e07d0dd24836f204773053fb4c
lejunyang <lejunyang@qq.com>
2024-02-20 11:05:20 +0800

    feat(utils): add some utils to extract values from style string

---
+++ packages/utils/src/browser/style.ts
@@ -0,0 +1,22 @@
+const styleCommentRE = /\/\*[^]*?\*\//g;
+
+export function removeStyleComment(style: string) {
+  return style.replace(styleCommentRE, '');
+}
+
+export function getValueFromStyle(style: string, key: string) {
+  style = removeStyleComment(style);
+  const reg = new RegExp(`${key}:\\s*([^;]+);?`);
+  const match = style.match(reg);
+  return match ? match[1].trim() : '';
+}
+
+export function getValuesFromStyle<K extends string[]>(style: string, ...keys: K): Record<K[number], string> {
+  style = removeStyleComment(style);
+  const result = {} as Record<K[number], string>;
+  const reg = new RegExp(`(${keys.join('|')}):\\s*([^;]+);?`, 'g');
+  for (const match of style.matchAll(reg)) {
+    result[match[1] as K[number]] = match[2].trim();
+  }
+  return result;
+}
