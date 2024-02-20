2f097321f1a0ee423435d7ec80c0600ec2224b0e
lejunyang <lejunyang@qq.com>
2024-02-20 11:19:59 +0800

    feat(core): add useInstanceStyle

--- packages/core/src/hooks/index.ts
+++ packages/core/src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from './breakpoints';
 export * from './createUseModel';
+export * from './instance';
 export * from './state';
 export * from './useClickOutside';
 export * from './useOverflowWatcher';
