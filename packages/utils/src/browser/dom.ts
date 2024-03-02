78f9151d8f8a0441f5da33e22ecfede94be583e6
lejunyang <lejunyang@qq.com>
2024-03-02 23:24:27 +0800

    feat(utils): add isHTMLInputElement, change node traverse methods

--- packages/utils/src/browser/dom.ts
+++ packages/utils/src/browser/dom.ts
@@ -2,13 +2,14 @@
 export function createGetNodeInTree<T extends Node | Element>({
   getNext,
   getParent,
-  getNextFromParent,
 }: {
   getNext: (i: T) => T | null | undefined;
   getParent: (i: T) => T | null | undefined;
-  getNextFromParent: (p: T) => T | null | undefined;
 }) {
-  return function (el?: T | null, options?: { isMatch?: (el: T) => boolean; shouldStop?: (el: T) => boolean }) {
+  return function (
+    el?: T | null,
+    options?: { isMatch?: (el: T, current: T) => boolean; shouldStop?: (el: T) => boolean },
+  ) {
     if (!el) return null;
     let isMatch = options?.isMatch || ((el) => !!el);
     let shouldStop = options?.shouldStop || ((el) => el.parentElement === document.documentElement);
@@ -16,16 +17,16 @@ export function createGetNodeInTree<T extends Node | Element>({
     while (true) {
       const next = getNext(temp);
       if (next) {
-        if (isMatch(next)) return next;
+        if (isMatch(next, temp)) return next;
         else temp = next;
       } else {
         const parent = getParent(temp);
         if (!parent || shouldStop(parent)) return null;
-        else if (isMatch(parent)) return parent;
+        else if (isMatch(parent, temp)) return parent;
         else {
-          const nextFromParent = getNextFromParent(parent);
+          const nextFromParent = getNext(parent);
           if (!nextFromParent) temp = parent;
-          else if (isMatch(nextFromParent)) return nextFromParent;
+          else if (isMatch(nextFromParent, temp)) return nextFromParent;
           else temp = nextFromParent;
         }
       }
@@ -34,48 +35,38 @@ export function createGetNodeInTree<T extends Node | Element>({
 }
 
 export const getPreviousMatchElInTree = createGetNodeInTree<Element>({
-  getNext: (e) => e.previousElementSibling,
-  getParent: (e) => e.assignedSlot || e.parentElement || (e.parentNode as ShadowRoot)?.host,
-  getNextFromParent: (p) => {
-    const prevEl = p.previousElementSibling;
-    if (!prevEl) return;
-    // will try to access shadowRoot, but not deeply iterate, useful for root element in shadow dom
-    if (prevEl.shadowRoot) return prevEl.shadowRoot.lastElementChild;
-    return prevEl.lastElementChild;
+  getNext: (e) => {
+    const pes = e.previousElementSibling;
+    if (!pes) return;
+    // will try to access shadowRoot and child, but not deeply iterate, useful for root element in shadow dom
+    return pes.shadowRoot?.lastElementChild || pes.lastElementChild || pes;
   },
+  getParent: (e) => e.assignedSlot || e.parentElement || (e.parentNode as ShadowRoot)?.host,
 });
 
 export const getPreviousMatchNodeInTree = createGetNodeInTree<Node>({
-  getNext: (e) => e.previousSibling,
-  getParent: (e) => (e as Element).assignedSlot || e.parentNode || (e as ShadowRoot)?.host,
-  getNextFromParent: (p) => {
-    const prevEl = p.previousSibling;
-    if (!prevEl) return;
-    const { shadowRoot } = prevEl as Element;
-    if (shadowRoot) return shadowRoot.lastChild;
-    return prevEl.lastChild;
+  getNext: (e) => {
+    const ps = e.previousSibling;
+    if (!ps) return;
+    return (ps as Element).shadowRoot?.lastChild || ps.lastChild || ps;
   },
+  getParent: (e) => (e as Element).assignedSlot || e.parentNode || (e as ShadowRoot)?.host,
 });
 
 export const getNextMatchElInTree = createGetNodeInTree<Element>({
-  getNext: (e) => e.nextElementSibling,
-  getParent: (e) => e.assignedSlot || e.parentElement || (e.parentNode as ShadowRoot)?.host,
-  getNextFromParent: (p) => {
-    const nextEl = p.nextElementSibling;
-    if (!nextEl) return;
-    if (nextEl.shadowRoot) return nextEl.shadowRoot.firstElementChild;
-    return nextEl.firstElementChild;
+  getNext: (e) => {
+    const nes = e.nextElementSibling;
+    if (!nes) return;
+    return nes.shadowRoot?.firstElementChild || nes.firstElementChild || nes;
   },
+  getParent: (e) => e.assignedSlot || e.parentElement || (e.parentNode as ShadowRoot)?.host,
 });
 
 export const getNextMatchNodeInTree = createGetNodeInTree<Node>({
-  getNext: (e) => e.nextSibling,
-  getParent: (e) => (e as Element).assignedSlot || e.parentNode || (e as ShadowRoot)?.host,
-  getNextFromParent: (p) => {
-    const nextEl = p.nextSibling;
-    if (!nextEl) return;
-    const { shadowRoot } = nextEl as Element;
-    if (shadowRoot) return shadowRoot.firstChild;
-    return nextEl.firstChild;
+  getNext: (e) => {
+    const ns = e.nextSibling;
+    if (!ns) return;
+    return (ns as Element).shadowRoot?.firstChild || ns.firstChild || ns;
   },
+  getParent: (e) => (e as Element).assignedSlot || e.parentNode || (e as ShadowRoot)?.host,
 });
