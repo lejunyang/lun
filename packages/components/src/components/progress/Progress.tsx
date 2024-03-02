ed2667e2c8780f01ec01645020c73a074458bc9b
lejunyang <lejunyang@qq.com>
2024-03-02 23:30:13 +0800

    feat(components/progress): add some props

--- packages/components/src/components/progress/Progress.tsx
+++ packages/components/src/components/progress/Progress.tsx
@@ -12,29 +12,35 @@ export const Progress = defineSSRCustomElement({
     const ns = useNamespace(name);
 
     const getInner = () => {
-      const { type } = props;
+      const { type, strokeColor } = props;
       switch (type) {
         case 'wave':
-          return <div class={ns.e('wave')}></div>;
+          return <div class={ns.e('wave')} style={{ backgroundColor: strokeColor }}></div>;
       }
     };
 
     const percent = computed(() => (+props.value! || 0) + '%');
 
     const rootStyles = computed(() => {
-      const { type } = props;
+      const { type, strokeColor } = props;
       switch (type) {
         case 'wave':
-          return ns.v({
-            value: percent.value,
-          });
+          return {
+            backgroundColor: strokeColor,
+            ...ns.v({
+              value: percent.value,
+            }),
+          };
       }
     });
 
     return () => {
+      const { noPercent } = props;
       return (
         <div class={ns.t} part="root" style={rootStyles.value}>
-          <slot>{percent.value}</slot>
+          <span class={ns.e('text')} part="text">
+            <slot>{!noPercent && percent.value}</slot>
+          </span>
           {getInner()}
         </div>
       );
