560c84d396c437670889c645d060bae5e389051f
lejunyang <lejunyang@qq.com>
2024-03-09 00:02:41 +0800

    feat(components/message): add fours static methods for message

--- packages/components/src/components/message/message.static-methods.ts
+++ packages/components/src/components/message/message.static-methods.ts
@@ -1,16 +1,22 @@
 import { getElementFirstName, getFirstThemeProvider, toElement } from 'utils';
 import { MessageOpenConfig, MessageProps } from './type';
 import { iMessage } from './Message';
+import { isString } from '@lun/utils';
+import { Status } from 'common';
 
 let message: iMessage;
 
+const transformConfig = (config: string | Omit<MessageStaticMethodParams, 'type'>, type: Status) => {
+  if (isString(config)) return { message: config, type };
+  else return { ...config, type };
+};
+
+export type MessageStaticMethodParams = MessageOpenConfig & { getContainer?: () => Element | string } & Pick<
+    MessageProps,
+    'placement' | 'offset'
+  >;
 export const methods = {
-  open({
-    getContainer,
-    placement,
-    offset,
-    ...config
-  }: MessageOpenConfig & { getContainer?: () => Element | string } & Pick<MessageProps, 'placement' | 'offset'> = {}) {
+  open({ getContainer, placement, offset, ...config }: MessageStaticMethodParams = {}) {
     if (!message || !message.isConnected) {
       const container = (getContainer && toElement(getContainer())) || getFirstThemeProvider() || document.body;
       const messageName = getElementFirstName('message')!;
@@ -24,6 +30,18 @@ export const methods = {
     });
     message.open(config);
     return message;
+  },
+  success(config: string | Omit<MessageStaticMethodParams, 'type'>) {
+    return methods.open(transformConfig(config, 'success'));
+  },
+  error(config: string | Omit<MessageStaticMethodParams, 'type'>) {
+    return methods.open(transformConfig(config, 'error'));
+  },
+  warning(config: string | Omit<MessageStaticMethodParams, 'type'>) {
+    return methods.open(transformConfig(config, 'warning'));
+  },
+  info(config: string | Omit<MessageStaticMethodParams, 'type'>) {
+    return methods.open(transformConfig(config, 'info'));
   },
   close(key: string | number) {
     if (message) message.close(key);
