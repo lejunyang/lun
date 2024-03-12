9159152bf608ba4c8d904dba529a8fb7ebe835d5
lejunyang <lejunyang@qq.com>
2024-03-12 17:39:56 +0800

    feat(components): autoDefine, remove allDefineFunctions

--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -28,84 +28,100 @@ import { defineProgress } from './progress/Progress';
 import { defineTextarea } from './textarea/Textarea';
 import { defineTeleportHolder } from './teleport-holder/TeleportHolder';
 import { supportCustomElement } from '@lun/utils';
-// import { GlobalStaticConfig } from './config';
+import { GlobalStaticConfig, components } from './config';
 
 export function importAllBasicStyles() {
   importIconStyle();
   importPopoverStyle();
 }
 
-export const allDefineFunctions = {
-  defineThemeProvider,
-  defineWatermark,
-  defineTeleportHolder,
-  defineForm,
-  defineFormItem,
-  defineButton,
-  defineMessage,
-  defineCallout,
-  defineCheckboxGroup,
-  defineCheckbox,
-  defineCustomRenderer,
-  defineDialog,
-  defineDivider,
-  defineIcon,
-  defineInput,
-  definePopover,
-  defineRadioGroup,
-  defineRadio,
-  defineSelect,
-  defineSelectOptgroup,
-  defineSelectOption,
-  defineSpin,
-  defineSwitch,
-  defineTag,
-  defineTooltip,
-  defineFilePicker,
-  defineProgress,
-  defineTextarea,
-};
-
 export function defineAllComponents() {
   if (!supportCustomElement) return;
   // found that provide and inject are strongly rely on the dom order of the elements, as in defineCustomElement parent is searched by parentNode and instanceof
   // In SSR, if elements are already in the document and the parent is defined after the child, it will not work
   // So we need to define the providers first
-  Object.values(allDefineFunctions).forEach((define) => define());
+  defineWatermark();
+  defineThemeProvider();
+  defineTeleportHolder();
+  defineForm();
+  defineFormItem();
+  defineButton();
+  defineMessage();
+  defineCallout();
+  defineCheckboxGroup();
+  defineCheckbox();
+  defineCustomRenderer();
+  defineDialog();
+  defineDivider();
+  defineIcon();
+  defineInput();
+  definePopover();
+  defineRadioGroup();
+  defineRadio();
+  defineSelect();
+  defineSelectOptgroup();
+  defineSelectOption();
+  defineSpin();
+  defineSwitch();
+  defineTag();
+  defineTooltip();
+  defineFilePicker();
+  defineProgress();
+  defineTextarea();
 }
 
-// export function autoDefine() {
-//   const discover = (root: Element) => {
-//     const rootTagName = root.tagName.toLowerCase();
-//     const { namespace, defaultProps } = GlobalStaticConfig;
-//     const prefix = namespace + '-';
-//     const rootIsLunElement = rootTagName.startsWith(prefix);
-//     const reg = new RegExp(`^${prefix}`);
-//     const componentNames = [...root.querySelectorAll(':not(:defined)')].flatMap((el) => {
-//       const name = el.tagName.toLowerCase().replace(reg, '');
-//       return name in defaultProps ? [name] : [];
-//     });
+let autoDefineCalled = false;
+export function autoDefine() {
+  if (autoDefineCalled) return;
+  autoDefineCalled = true;
+  const { namespace, defaultProps } = GlobalStaticConfig;
+  const undefinedSet = new Set();
+  components.forEach((comp) => {
+    const tagName = namespace + '-' + comp;
+    if (!customElements.get(tagName)) {
+      undefinedSet.add(comp);
+      customElements.whenDefined(tagName).then(() => {
+        undefinedSet.delete(comp);
+        if (!undefinedSet.size) {
+          observer.disconnect();
+        }
+      });
+    }
+  });
+  const discover = (root: Element) => {
+    const rootTagName = root.tagName.toLowerCase();
+    const prefix = namespace + '-';
+    const rootIsLunElement = rootTagName.startsWith(prefix);
+    const reg = new RegExp(`^${prefix}`);
+    const componentNames = [...root.querySelectorAll(':not(:defined)')].flatMap((el) => {
+      const name = el.tagName.toLowerCase().replace(reg, '');
+      return name in defaultProps ? [name] : [];
+    });
 
-//     if (rootIsLunElement && !customElements.get(rootTagName)) {
-//       componentNames.unshift(rootTagName.replace(reg, ''));
-//     }
-//     const componentsToRegister = [...new Set(componentNames)];
-//     return Promise.allSettled(componentsToRegister.map((componentName) => {
-
-//     }));
-//   };
-//   const observer = new MutationObserver((mutations) => {
-//     for (const { addedNodes } of mutations) {
-//       for (const node of addedNodes) {
-//         if (node.nodeType === Node.ELEMENT_NODE) {
-//           discover(node as Element);
-//         }
-//       }
-//     }
-//   });
-//   observer.observe(document.documentElement, { subtree: true, childList: true });
-//   discover(document.body);
-// }
+    if (rootIsLunElement && !customElements.get(rootTagName)) {
+      componentNames.unshift(rootTagName.replace(reg, ''));
+    }
+    const componentsToRegister = [...new Set(componentNames)];
+    return Promise.allSettled(
+      componentsToRegister.map((componentName) => {
+        return import(`./${componentName}/${componentName}.define.ts`).catch(() => {
+          throw new Error(`Unable to auto define component ${componentName}`);
+        });
+      }),
+    );
+  };
+  const observer = new MutationObserver((mutations) => {
+    for (const { addedNodes } of mutations) {
+      for (const node of addedNodes) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          discover(node as Element);
+        }
+      }
+    }
+  });
+  observer.observe(document.documentElement, { subtree: true, childList: true });
+  discover(document.body);
+}
 
 export * from './animation';
 export * from './button';
