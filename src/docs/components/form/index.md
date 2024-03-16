b0b5a1b55f03b2dee8344fdfb31d10f537d476f5
lejunyang <lejunyang@qq.com>
2024-03-16 11:13:24 +0800

    docs: form and input

--- src/docs/components/form/index.md
+++ src/docs/components/form/index.md
@@ -5,11 +5,41 @@ lang: zh-CN
 
 ## 表单管控
 
-表单由`useForm`产生的实例进行管控，该实例可被多个表单共享，该实例用于方便管理表单的数据、状态、方法以及事件。如果在`form`加载时未指定`instance`，内部会创建一个，该属性不可以动态指定。
+表单由`useForm`产生的实例进行管控，该实例可被多个表单共享，该实例用于方便管理表单的数据、状态、方法以及事件。如果在组件加载时未指定`instance`，内部会创建一个，该属性**不可以动态指定**。
 
-实例的属性和方法都会暴露到`form`DOM节点本身，ts类型过长暂不列出，你可以在控制台选中form节点，通过`dir($0)`查看有哪些属性和方法
+实例的属性和方法都会暴露到组件 DOM 节点本身，ts 类型过长暂不列出，你可以在控制台选中节点，通过`dir($0)`查看有哪些属性和方法
 
 <!-- @Code:useForm -->
+
+## 公共属性与默认插槽
+
+通过`itemProps`可以给表单下所有的`form-item`设置公共属性，`itemProps`还可以为函数以便于动态设置
+
+```ts
+type itemProps =
+  | Partial<Omit<FormItemSetupProps, 'deps'>>
+  | ((params: {
+      formContext: CollectorContext<FormSetupProps, FormItemSetupProps, FormProvideExtra> | undefined;
+      formItemProps: FormItemSetupProps;
+    }) => Partial<Omit<FormItemSetupProps, 'deps'>>);
+```
+
+`form-item`的默认插槽用于放置输入组件，只要配置了`name`属性，其下的输入组件便可以自动获取和设置表单的值（表单支持的组件可见左侧菜单“输入组件”）
+
+另外，`form-item`也可以通过`element`属性和`elementProps`属性直接设置其下渲染的组件，其中`elementProps`也可以设置为函数。如果设置了`element`，`form-item`的默认插槽下的内容便会渲染到输入组件的默认插槽下
+
+```ts
+type elementProps =
+  | object
+  | ((param: {
+      formContext: CollectorContext<FormProps, FormItemProps, FormProvideExtra> | undefined;
+      formItemProps: FormItemSetupProps;
+    }) => any);
+```
+
+动态的公共属性某些时候会非常有用，能够减少很多重复的设置，而单个字段也可由自己的属性覆盖，可以看下面的示例查看效果
+
+<!-- @Code:commonProps -->
 
 ## 所有表单组件
 
@@ -21,7 +51,7 @@ lang: zh-CN
 
 ## 依赖字段
 
-通过`deps`属性可设置该字段依赖的其他字段，其值为其他字段的 name 字符串或 name 数组。当其依赖字段的值发生变化后，可自动控制该字段清除、禁用、必输、校验，具体可通过以下属性进行控制
+`form-item`可通过`deps`属性可设置该字段依赖的其他字段，其值为其他字段的 name 字符串或 name 数组。当其依赖字段的值发生变化后，可自动控制该字段清除、禁用、必输、校验，具体可通过以下属性进行控制
 
 - `clearWhenDepChange`: 当其为`true`时，依赖字段的值变化后，本字段值清空
 - `disableWhenDepFalsy`: 控制当依赖的字段值什么情况下本字段禁用，可选值有：
@@ -40,7 +70,7 @@ lang: zh-CN
 - 设置数字的校验属性也可以设置为其他字段的值，可以这样设置的属性有`min`, `max`, `step`, `precision`, `moreThan`, `lessThan`, `len`
 
 :::info 说明
-下例中的字段“已使用”，其max="total"，意味着如果“总量”字段有值且为数字的时候，它的max取值则为“总量”的值，另外，下面所有的数字字段step和precision属性都直接设为了“布长”和“精度”字段的值
+下例中的字段“已使用”，其 max="total"，意味着如果“总量”字段有值且为数字的时候，它的 max 取值则为“总量”的值，另外，下面所有的数字字段 step 和 precision 属性都直接设为了“布长”和“精度”字段的值
 :::
 
 <!-- @Code:validate -->
--- src/docs/components/input/updateWhen.vue.tsx
+++ src/docs/components/input/updateWhen.vue.tsx
@@ -1,34 +1,39 @@
+import { useForm } from '@lun/core';
 import { ref } from 'vue';
 
 const input = ref('111');
 const mul = ref(['1']);
+const instance = useForm();
 export default () => {
   return (
     <>
-      <div>
-        updateWhen="input" <l-input v-update={input.value} updateWhen="input" />
-      </div>
-      <div>
-        updateWhen="not-composing" <l-input v-update={input.value} updateWhen="not-composing" />
-      </div>
-      <div>
-        updateWhen="change" <l-input v-update={input.value} updateWhen="change" />
-      </div>
-      <pre style="flex-basis: 100%">value: {input.value}</pre>
-      <div>
-        updateWhen="input" <l-input v-update={mul.value} updateWhen="input" multiple />
-      </div>
-      <div>
-        updateWhen="not-composing" <l-input v-update={mul.value} updateWhen="not-composing" multiple />
-      </div>
-      <div>
-        updateWhen="change" <l-input v-update={mul.value} updateWhen="change" multiple />
-      </div>
-      <div>
-        updateWhen=["change", "not-composing"]
-        <l-input v-update={mul.value} updateWhen={['change', 'not-composing']} multiple />
-      </div>
-      <pre style="flex-basis: 100%">multiple values: {JSON.stringify(mul.value)}</pre>
+      <pre style="flex-basis: 100%">value: {instance.formData.input}</pre>
+      <l-form cols={1} instance={instance}>
+        <l-form-item name="input" label={`updateWhen="input"`}>
+          <l-input updateWhen="input" />
+        </l-form-item>
+        <l-form-item name="input" label={`updateWhen="not-composing"`}>
+          <l-input updateWhen="not-composing" />
+        </l-form-item>
+        <l-form-item name="input" label={`updateWhen="change"`}>
+          <l-input updateWhen="change" />
+        </l-form-item>
+      </l-form>
+      <pre style="flex-basis: 100%">multiple values: {JSON.stringify(instance.formData.multi)}</pre>
+      <l-form cols={1} instance={instance}>
+        <l-form-item name="multi" label={`updateWhen="input"`}>
+          <l-input updateWhen="input" multiple />
+        </l-form-item>
+        <l-form-item name="multi" label={`updateWhen="not-composing"`}>
+          <l-input updateWhen="not-composing" multiple />
+        </l-form-item>
+        <l-form-item name="multi" label={`updateWhen="change"`}>
+          <l-input updateWhen="change" multiple />
+        </l-form-item>
+        <l-form-item name="multi" label={`updateWhen={['change', 'not-composing']}`}>
+          <l-input updateWhen={['change', 'not-composing']} multiple />
+        </l-form-item>
+      </l-form>
     </>
   );
 };
