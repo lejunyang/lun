c5072aebc96ca0704ee550d578b2e3439338dec7
lejunyang <lejunyang@qq.com>
2024-03-20 21:24:05 +0800

    feat(theme/mentions): styles for mention blocks

--- src/docs/components/mentions/basicUsage.vue.tsx
+++ src/docs/components/mentions/basicUsage.vue.tsx
@@ -3,5 +3,7 @@ import { options } from "data";
 export default () => (
   <>
     <l-mentions value="test@me what" options={options}></l-mentions>
+    <l-mentions value="test@me what" options={options} readonly></l-mentions>
+    <l-mentions value="test@me what" options={options} disabled></l-mentions>
   </>
 );
