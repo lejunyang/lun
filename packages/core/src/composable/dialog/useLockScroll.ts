e5155a1351e5e4c7452c0343f29d35aa494b4831
lejunyang <lejunyang@qq.com>
2024-04-02 21:56:34 +0800

    fix(core/useLockScroll): fix layout shift issue when there are fixed elements with percentage width

--- packages/core/src/composable/dialog/useLockScroll.ts
+++ packages/core/src/composable/dialog/useLockScroll.ts
@@ -1,37 +1,85 @@
-import { getScrollbarWidth, pick, supportScrollbarGutter } from '@lun/utils';
+import {
+  getScrollbarWidth,
+  getWindow,
+  isRootOrBody,
+  objectKeys,
+  pick,
+  setStyle,
+  supportScrollbarGutter,
+} from '@lun/utils';
 import { tryOnScopeDispose } from '../../hooks';
 
 const lockNumMap = new WeakMap<HTMLElement, number>(),
-  lockElStyleMap = new WeakMap<
-    HTMLElement,
-    { overflow: string; scrollbarGutter: string; paddingRight: string; paddingBottom: string }
-  >();
+  lockElStyleMap = new WeakMap<HTMLElement, Partial<CSSStyleDeclaration>>(),
+  scrollPositionMap = new WeakMap<HTMLElement, [number, number]>();
 export function useLockScroll() {
   const localLocks = new Set<HTMLElement>();
+
+  function storeAndSetStyle(el: HTMLElement, style: Partial<CSSStyleDeclaration>) {
+    lockElStyleMap.set(el, pick(el.style, objectKeys(style)));
+    setStyle(el, style, true);
+  }
+
   function lock(el: HTMLElement) {
     const prevNum = lockNumMap.get(el);
     if (prevNum) lockNumMap.set(el, prevNum + 1);
     else {
       lockNumMap.set(el, 1);
-      // store the original style
-      lockElStyleMap.set(el, pick(el.style, ['overflow', 'scrollbarGutter', 'paddingRight', 'paddingBottom']));
-      el.style.setProperty('overflow', 'hidden', 'important');
-      if (supportScrollbarGutter) el.style.setProperty('scrollbar-gutter', 'stable', 'important');
-      else {
-        const { paddingRight, paddingBottom } = getComputedStyle(el),
-          { y, x } = getScrollbarWidth(el);
-        el.style.setProperty('padding-right', `calc(${paddingRight} + ${y}px)`, 'important');
-        el.style.setProperty('padding-bottom', `calc(${paddingBottom} + ${x}px)`, 'important');
+      const { y } = getScrollbarWidth(el);
+
+      // found that overflow hidden + scrollbar-gutter stable(or padding, width, margin...) is not working on vitepress, it will still shift the layout
+      // because the aside menu is position: fixed, width: calc(100% - ...). its parent is not root html but the viewport. it'll get wider when we set overflow:hidden on html
+      // so we need to choose another way
+      if (isRootOrBody(el)) {
+        const {
+          document: { documentElement, body },
+          scrollX,
+          scrollY,
+        } = getWindow(el);
+        scrollPositionMap.set(el, [scrollX, scrollY]);
+        // do not add scrollY on html's top or translate, or it will affect fixed children elements, we set it on body
+        storeAndSetStyle(documentElement, {
+          position: 'fixed',
+          top: `0`,
+          left: '0',
+          bottom: '0',
+          right: '0',
+          transform: 'translate(0,0)', // to let it become parent of those fixed
+        });
+        storeAndSetStyle(body, {
+          position: 'relative',
+          top: `-${scrollY}px`,
+          left: scrollX + 'px',
+          right: y + 'px',
+        });
+      } else {
+        storeAndSetStyle(el, {
+          overflow: 'hidden',
+          scrollbarGutter: supportScrollbarGutter ? 'stable' : undefined,
+          paddingRight: `calc(${getComputedStyle(el).paddingRight} + ${y}px)`,
+        });
       }
     }
     localLocks.add(el);
   }
+
+  function restoreElStyle(el: HTMLElement) {
+    const style = lockElStyleMap.get(el);
+    if (style) Object.assign(el.style, style);
+  }
+
   function unlock(el: HTMLElement, force?: boolean) {
     const num = lockNumMap.get(el);
     localLocks.delete(el);
     if (num === 1 || force) {
-      // restore the style
-      Object.assign(el.style, lockElStyleMap.get(el)!);
+      if (isRootOrBody(el)) {
+        const elWindow = getWindow(el);
+        const { documentElement, body } = elWindow.document;
+        restoreElStyle(documentElement);
+        restoreElStyle(body);
+        const [scrollX, scrollY] = scrollPositionMap.get(el)!;
+        elWindow.scrollTo(scrollX, scrollY);
+      } else restoreElStyle(el);
       lockNumMap.delete(el);
     } else if (num) lockNumMap.set(el, num - 1);
   }
