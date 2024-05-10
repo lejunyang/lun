a93922516043f1a41cde4462eca9f7c24122a781
lejunyang <lejunyang@qq.com>
2024-05-10 11:27:00 +0800

    chore: update doc generate and link script, exec this script when build

--- utils/generateAndLinkDocs.js
+++ utils/generateAndLinkDocs.js
@@ -2,19 +2,30 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-async function createSymlinks(src, dest) {
+const comment = `<!--this file is copied from chinese md, remove this comment to update it, or it will be overwritten when next build-->`;
+const noSymlink = process.argv.slice(2).some((i) => i.includes('no-symlink'));
+
+async function gen(src, dest) {
   try {
     await fs.mkdir(dest, { recursive: true }); // make sure dest dir exists
     const entries = await fs.readdir(src, { withFileTypes: true });
     for (const entry of entries) {
       const srcPath = path.join(src, entry.name);
-      const destPath = path.join(dest, entry.name);
+      const destPath = path.join(dest, entry.name),
+        destStat = await fs.lstat(destPath).catch(() => null);
+      const destIsSymlink = destStat?.isSymbolicLink();
       if (entry.isDirectory()) {
-        await createSymlinks(srcPath, destPath);
+        await gen(srcPath, destPath);
       } else if (entry.name.endsWith('.md')) {
-        // copy md file
-        await fs.copyFile(srcPath, destPath);
-      } else {
+        const srcContent = await fs.readFile(srcPath, 'utf-8');
+        if (destStat) {
+          const destContent = await fs.readFile(destPath, 'utf-8');
+          if (!destContent.startsWith(comment)) {
+            continue;
+          }
+        }
+        fs.writeFile(destPath, `${comment}\n${srcContent}`);
+      } else if (!noSymlink && (destIsSymlink === true || destIsSymlink == null)) {
         await fs.symlink(srcPath, destPath);
       }
     }
@@ -32,12 +43,11 @@ const __dirname = path.dirname(__filename); // __dirname is not defined in ES mo
 fs.readdir(path.join(__dirname, '../src/docs'), { withFileTypes: true }).then((entries) => {
   for (const entry of entries) {
     if (excludes.has(entry.name)) continue;
-    console.log('entries.name', entry.name);
     if (entry.isDirectory() && !langs.has(entry.name)) {
       for (const lang of langs) {
         const src = path.join(__dirname, `../src/docs/${entry.name}`),
           dest = path.join(__dirname, `../src/docs/${lang}/${entry.name}`);
-        createSymlinks(src, dest);
+        gen(src, dest);
       }
     }
   }
