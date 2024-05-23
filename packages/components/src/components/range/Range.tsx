bf3c8206386ce814ceba255f74533693d99b33d1
lejunyang <lejunyang@qq.com>
2024-05-23 17:16:33 +0800

    feat(components/range): support using arrow to step down and up

--- packages/components/src/components/range/Range.tsx
+++ packages/components/src/components/range/Range.tsx
@@ -4,7 +4,15 @@ import { rangeEmits, rangeProps } from './type';
 import { computed } from 'vue';
 import { useNamespace, useValueModel } from 'hooks';
 import { useSetupEdit, useSetupEvent } from '@lun/core';
-import { at, toArrayIfNotNil } from '@lun/utils';
+import {
+  at,
+  isArray,
+  isArrowDownEvent,
+  isArrowLeftEvent,
+  isArrowRightEvent,
+  isArrowUpEvent,
+  toArrayIfNotNil,
+} from '@lun/utils';
 import { GlobalStaticConfig } from '../config/config.static';
 
 const name = 'range';
@@ -19,31 +27,77 @@ export const Range = defineSSRCustomElement({
     const [editComputed] = useSetupEdit();
     const valueModel = useValueModel(props);
 
-    const { ensureNumber, min, max, minus, divide, toRawNum } = GlobalStaticConfig.math;
+    const { ensureNumber, min, max, minus, divide, toRawNum, plus } = GlobalStaticConfig.math;
+    type BigNum = ReturnType<typeof ensureNumber>;
+    type CanBeNum = string | number;
     const minVal = computed(() => ensureNumber(props.min, 0));
-    const maxVal = computed(() => ensureNumber(props.min, 100));
-    const getPercent = (val: string | number) => {
+    const maxVal = computed(() => ensureNumber(props.max, 100));
+    const len = computed(() => minus(maxVal.value, minVal.value));
+    const step = computed(() => ensureNumber(props.step, 1));
+    const clamp = (val: CanBeNum) => {
       const vmin = minVal.value,
         vmax = maxVal.value;
-      const v = min(max(ensureNumber(val, 0), vmin), vmax);
-      return toRawNum(divide(minus(v, vmin), minus(vmax, vmin)));
+      return min(max(ensureNumber(val, 0), vmin), vmax);
     };
-    const percents = computed(() => {
+    const getPercent = (val: BigNum) => {
+      return toRawNum(divide(minus(val, minVal.value), len.value));
+    };
+
+    const processedValues = computed(() => {
       const { value } = valueModel;
       return toArrayIfNotNil(value ?? 0)
-        .map(getPercent)
-        .sort((a, b) => a - b);
+        .map((v) => {
+          const clamped = clamp(v),
+            p = getPercent(clamped);
+          return [clamped, p] as const;
+        })
+        .sort((a, b) => a[1] - b[1]);
     });
 
+    const updateVal = (index: number, val: BigNum) => {
+      const res = processedValues.value.map((v, i) => {
+        return i === index ? toRawNum(val) : toRawNum(v[0]);
+      });
+      if (res.length === 1 && !isArray(valueModel.value)) valueModel.value = res[0];
+      else valueModel.value = res;
+    };
+
+    const handlers = {
+      onKeydown(e: KeyboardEvent) {
+        const { target } = e;
+        const {
+          dataset: { index },
+        } = target as HTMLElement;
+        if (index == null) return;
+        const value = processedValues.value[+index][0];
+        if (isArrowLeftEvent(e) || isArrowDownEvent(e)) {
+          updateVal(+index, max(minus(value, step.value), minVal.value));
+        } else if (isArrowRightEvent(e) || isArrowUpEvent(e)) {
+          updateVal(+index, min(plus(value, step.value), maxVal.value));
+        }
+      },
+    };
+
     return () => {
-      const { value } = percents;
+      const { value } = processedValues;
       return (
-        <span class={ns.t} part={ns.p('part')} style={ns.v({ min: at(value, 0), max: at(value, -1) })}>
+        <div
+          class={ns.t}
+          part={ns.p('root')}
+          style={ns.v({ min: value.length > 1 ? at(value, 0)[1] : 0, max: at(value, -1)[1] })}
+          {...handlers}
+        >
           <span class={ns.e('track')} part={ns.p('track')}></span>
-          {value.map((v) => (
-            <span class={ns.e('handle')} part={ns.p('handle')} style={ns.v({ percent: v })}></span>
+          {value.map(([_, p], index) => (
+            <span
+              data-index={index}
+              class={ns.e('handle')}
+              part={ns.p('handle')}
+              style={ns.v({ percent: p })}
+              tabindex={0}
+            ></span>
           ))}
-        </span>
+        </div>
       );
     };
   },
--- src/docs/components/range/basicUsage.vue.tsx
+++ src/docs/components/range/basicUsage.vue.tsx
@@ -1,7 +1,7 @@
 export default () => {
   return (
     <>
-      <l-range></l-range>
+      <l-range value={30}></l-range>
     </>
   );
 };
