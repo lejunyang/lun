a950dd0a1af5f18c5827b5574bbfd5fcb8ba169f
lejunyang <lejunyang@qq.com>
2024-06-05 21:31:47 +0800

    feat(theme): styles for labels; add readonly style; improve margin

--- src/docs/components/range/basicUsage.vue.tsx
+++ src/docs/components/range/basicUsage.vue.tsx
@@ -2,8 +2,10 @@ export default () => {
   return (
     <>
       <l-range value={30}></l-range>
+      <l-range value={30} readonly></l-range>
       <l-range value={30} disabled></l-range>
       <l-range value={30} type="vertical"></l-range>
+      <l-range value={30} type="vertical" readonly></l-range>
       <l-range value={30} type="vertical" disabled></l-range>
     </>
   );
---
+++ src/docs/components/range/labels.vue.tsx
@@ -0,0 +1,6 @@
+export default () => (
+  <>
+    <l-range labels={{ 0: '0%', 30: '30%', 100: '100%' }}></l-range>
+    <l-range type="vertical" labels={{ 10: '10%', 40: '40%', 100: '100%' }}></l-range>
+  </>
+);
