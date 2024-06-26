cb5d211ed9c44ff8ab477318e89b5b583246d07a
lejunyang <lejunyang@qq.com>
2024-06-26 16:10:00 +0800

    feat(core/presets): add type methods group, add processType logic, add isSame method

--- packages/core/src/presets/date.ts
+++ packages/core/src/presets/date.ts
@@ -1,4 +1,4 @@
-import { assignIfNil, identity } from '@lun/utils';
+import { assignIfNil, identity, objectKeys } from '@lun/utils';
 
 export type DateMethods<T> = {
   // get
@@ -51,12 +51,54 @@ export type DateMethods<T> = {
   };
 
   // optional methods
-  add?: (date: T, diff: number, type: 'y' | 'M' | 'd') => T;
-  set?: (date: T, val: number, type: 'y' | 'M' | 'd' | 'h' | 'm' | 's') => T;
+  type?: {
+    add?: (type: DateType, date: T, diff: number) => T;
+    set?: (type: DateType, date: T, val: number) => T;
+    isSame?: (type: DateType, date1: T, date2: T) => boolean;
+  };
+};
+
+type BaseDateType = 'y' | 'M' | 'd' | 'h' | 'm' | 's';
+export type DateType =
+  | BaseDateType
+  | 'year'
+  | 'years'
+  | 'month'
+  | 'months'
+  | 'day'
+  | 'days'
+  | 'hour'
+  | 'hours'
+  | 'minute'
+  | 'minutes'
+  | 'second'
+  | 'seconds';
+
+const processType = (type: string) => {
+  let lType: string;
+  return (
+    type.length === 1 ? type : ((lType = type.toLowerCase()), lType.startsWith('mo') ? 'M' : lType[0])
+  ) as BaseDateType;
 };
 
 export const createDate = <T>(methods: DateMethods<T>) => {
-  const { addYear, addMonth, addDate, setYear, setMonth, setDate, setHour, setMinute, setSecond } = methods;
+  const {
+    addYear,
+    addMonth,
+    addDate,
+    setYear,
+    setMonth,
+    setDate,
+    setHour,
+    setMinute,
+    setSecond,
+    getYear,
+    getMonth,
+    getDate,
+    getHour,
+    getMinute,
+    getSecond,
+  } = methods;
   const addMap = {
       y: addYear,
       M: addMonth,
@@ -69,13 +111,36 @@ export const createDate = <T>(methods: DateMethods<T>) => {
       h: setHour,
       m: setMinute,
       s: setSecond,
+    },
+    getMap = {
+      y: getYear,
+      M: getMonth,
+      d: getDate,
+      h: getHour,
+      m: getMinute,
+      s: getSecond,
     };
-  return assignIfNil(methods, {
-    add(date, diff, type) {
-      return (addMap[type] || identity)(date, diff);
-    },
-    set(date, val, type) {
-      return (setMap[type] || identity)(date, val);
-    },
+  assignIfNil(methods, {
+    type: {},
   });
+  assignIfNil(methods.type!, {
+    add(type, date, diff) {
+      // @ts-ignore // TODO
+      return (addMap[processType(type)] || identity)(date, diff);
+    },
+    set(type, date, val) {
+      return (setMap[processType(type)] || identity)(date, val);
+    },
+    isSame(type, date1, date2) {
+      // TODO milliseconds
+      const t = processType(type);
+      if (!getMap[t] || !date1 || !date2) return false;
+      for (const k of objectKeys(getMap)) {
+        if (getMap[k](date1) !== getMap[k](date2)) return false;
+        if (k === t) return true;
+      }
+      return false;
+    },
+  } satisfies Required<DateMethods<T>['type'] & {}>);
+  return methods as Omit<DateMethods<T>, 'type'> & { type: Required<DateMethods<T>['type'] & {}> };
 };
