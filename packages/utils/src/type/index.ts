25fefccb0f0c93c294816200e9589844cdbcb8ac
lejunyang <lejunyang@qq.com>
2024-06-28 10:28:47 +0800

    feat(utils): add some types

--- packages/utils/src/type/index.ts
+++ packages/utils/src/type/index.ts
@@ -29,3 +29,33 @@ export type ExcludeNumberAndSymbol<T> = T extends number | symbol ? never : T;
 export type TryGet<O, K, D = never> = K extends keyof O ? O[K] : D;
 
 export type RemoveFirstParam<F> = F extends (a: any, ...args: infer P) => infer R ? (...args: P) => R : F;
+
+export type DeepPartial<T extends object> = {
+  [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
+};
+
+export type DeepRequired<T extends object> = {
+  [K in keyof T]-?: T[K] extends object ? DeepRequired<T[K]> : T[K];
+};
+
+export type DeepReadonly<T extends object> = {
+  readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
+};
+
+export type DeepWritable<T extends object> = {
+  -readonly [K in keyof T]: T[K] extends object ? DeepWritable<T[K]> : T[K];
+};
+
+export type DeepNonNullable<T> = T extends object
+  ? {
+      [K in keyof T]: DeepNonNullable<T[K]>;
+    }
+  : NonNullable<T>;
+
+export type GetRequiredKeys<T extends object> = {
+  [K in keyof T]-?: undefined extends T[K] ? never : K;
+}[keyof T];
+
+export type GetOptionalKeys<T extends object> = {
+  [K in keyof T]-?: undefined extends T[K] ? K : never;
+}[keyof T];
