987e854aa5affc686acd9514a72e8186898f1a78
lejunyang <lejunyang@qq.com>
2024-07-01 20:21:18 +0800

    feat(components/icon): add default left, right, double-right icons

--- packages/components/src/components/icon/icon.default.tsx
+++ packages/components/src/components/icon/icon.default.tsx
@@ -1,3 +1,4 @@
+import { JSX } from 'vue/jsx-runtime';
 import { IconLibrary } from './icon.registry';
 
 const commonProps = {
@@ -19,22 +20,31 @@ const warningPath =
 const getDblLeftD = (m: number, l: number) =>
   `M${m}.354 1.646a.5.5 0 0 1 0 .708L${l}.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0`;
 
+const getDown = (attrs: any, transform?: string) => (
+  <svg {...attrs} {...commonProps} transform={transform}>
+    <path
+      fill-rule="evenodd"
+      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+    />
+  </svg>
+);
+
+const getDblLeft = (attrs: any, transform?: string) => (
+  <svg {...attrs} {...commonProps} transform={transform}>
+    <path fill-rule="evenodd" d={getDblLeftD(8, 2)} />
+    <path fill-rule="evenodd" d={getDblLeftD(12, 6)} />
+  </svg>
+);
+
 const icons = {
-  clear: (attrs: any) => (
+  clear: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
       <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
     </svg>
   ),
-  down: (attrs: any) => (
-    <svg {...attrs} {...commonProps}>
-      <path
-        fill-rule="evenodd"
-        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-      />
-    </svg>
-  ),
-  up: (attrs: any) => (
+  down: (attrs) => getDown(attrs),
+  up: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path
         fill-rule="evenodd"
@@ -42,7 +52,9 @@ const icons = {
       />
     </svg>
   ),
-  plus: (attrs: any) => (
+  left: (attrs) => getDown(attrs, 'rotate(90)'),
+  right: (attrs) => getDown(attrs, 'rotate(-90)'),
+  plus: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path
         fill-rule="evenodd"
@@ -50,17 +62,17 @@ const icons = {
       />
     </svg>
   ),
-  minus: (attrs: any) => (
+  minus: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8" />
     </svg>
   ),
-  x: (attrs: any) => (
+  x: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
     </svg>
   ),
-  check: (attrs: any) => (
+  check: (attrs) => (
     <svg {...attrs} {...commonProps} viewBox="0 0 9 9">
       <path
         fill-rule="evenodd"
@@ -69,7 +81,7 @@ const icons = {
       ></path>
     </svg>
   ),
-  dash: (attrs: any) => (
+  dash: (attrs) => (
     <svg {...attrs} {...commonProps} viewBox="0 0 1024 1024">
       <path
         d="M984.615385 541.538462c0 15.753846-13.784615 29.538462-29.538462 29.538461h-886.153846c-15.753846 0-29.538462-13.784615-29.538462-29.538461v-59.076924c0-15.753846 13.784615-29.538462 29.538462-29.538461h886.153846c15.753846 0 29.538462 13.784615 29.538462 29.538461v59.076924z"
@@ -77,67 +89,63 @@ const icons = {
       ></path>
     </svg>
   ),
-  success: (attrs: any) => (
+  success: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d={commonCirclePath + successPath} />
     </svg>
   ),
-  'success-no-circle': (attrs: any) => (
+  'success-no-circle': (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d={'M16 8' + successPath} />
     </svg>
   ),
-  error: (attrs: any) => (
+  error: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d={commonCirclePath + errorPath} />
     </svg>
   ),
-  'error-no-circle': (attrs: any) => (
+  'error-no-circle': (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d={errorPath} />
     </svg>
   ),
-  warning: (attrs: any) => (
+  warning: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d={commonCirclePath + warningPath} />
     </svg>
   ),
-  'warning-no-circle': (attrs: any) => (
+  'warning-no-circle': (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d={warningPath} />
     </svg>
   ),
-  info: (attrs: any) => (
+  info: (attrs) => (
     <svg {...attrs} {...commonProps} viewBox="64 64 896 896">
       <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm32 664c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path>
     </svg>
   ),
-  help: (attrs: any) => (
+  help: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
       <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94" />
     </svg>
   ),
-  eye: (attrs: any) => (
+  eye: (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
       <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
     </svg>
   ),
-  'eye-slash': (attrs: any) => (
+  'eye-slash': (attrs) => (
     <svg {...attrs} {...commonProps}>
       <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
       <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
       <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
     </svg>
   ),
-  'double-left': (attrs: any) => (
-    <svg {...attrs} {...commonProps}>
-      <path fill-rule="evenodd" d={getDblLeftD(8, 2)} />
-      <path fill-rule="evenodd" d={getDblLeftD(12, 6)} />
-    </svg>
-  ),
-} as const;
+  'double-left': (attrs) => getDblLeft(attrs),
+  'double-right': (attrs) => getDblLeft(attrs, 'rotate(180)'),
+} satisfies Record<string, (attrs: any) => JSX.Element>;
 
 export const defaultIconLibrary: IconLibrary = {
   library: 'default',
