018227d4cf5da9e1fe782c57f92edfd587b70050
lejunyang <lejunyang@qq.com>
2024-08-03 17:00:49 +0800

    feat(components/tabs): add activeSlot and defaultActiveSlot, add disabled in tab-item, add tab update logic

--- packages/components/src/components/tabs/Tabs.tsx
+++ packages/components/src/components/tabs/Tabs.tsx
@@ -2,28 +2,58 @@ import { defineSSRCustomElement } from 'custom';
 import { createDefineElement } from 'utils';
 import { tabsEmits, tabsProps } from './type';
 import { defineIcon } from '../icon/Icon';
-import { TransitionGroup, computed, ref, vShow, withDirectives } from 'vue';
+import { TransitionGroup, computed, ref, watchEffect } from 'vue';
 import { useNamespace } from 'hooks';
 import { getCompParts, getTransitionProps } from 'common';
 import { isArray } from '@lun/utils';
 import { renderCustom } from '../custom-renderer/CustomRenderer';
+import { useSetupEvent } from '@lun/core';
 
 const name = 'tabs';
-const parts = ['root', 'nav', 'content', 'wrapper', 'panel'] as const;
+const parts = ['root', 'nav', 'content', 'wrapper', 'panel', 'tab'] as const;
 const compParts = getCompParts(name, parts);
 export const Tabs = defineSSRCustomElement({
   name,
   props: tabsProps,
   emits: tabsEmits,
-  setup(props) {
+  setup(props, { emit: e }) {
     const ns = useNamespace(name);
-    const activeKey = ref();
+    const emit = useSetupEvent<typeof e>();
+    const localActive = ref();
     const showedKeys = new Set();
+    let controlled = false;
 
-    const tabs = computed(() => (isArray(props.items) ? props.items.filter(Boolean) : []));
+    const tabs = computed(() =>
+      isArray(props.items)
+        ? props.items
+            .map((t, i) => {
+              const slot = String(t.slot ?? i);
+              return {
+                ...t,
+                slot,
+                onClick: () => {
+                  if (!t.disabled) {
+                    emit('update', slot);
+                    if (!controlled) {
+                      localActive.value = slot;
+                      showedKeys.add(slot);
+                    }
+                  }
+                },
+              };
+            })
+            .filter(Boolean)
+        : [],
+    );
+    watchEffect(() => {
+      const { activeSlot, defaultActiveSlot } = props;
+      if ((controlled = activeSlot != null)) localActive.value = activeSlot;
+      else if (defaultActiveSlot != null) localActive.value = defaultActiveSlot;
+      else localActive.value = tabs.value[0]?.slot ?? '0';
+      showedKeys.add(localActive.value);
+    });
     return () => {
       const { destroyInactive, forceRender } = props;
-      const { value: key } = activeKey;
       const transitionAttrs = {
         tag: 'div',
         class: ns.e('content'),
@@ -34,24 +64,28 @@ export const Tabs = defineSSRCustomElement({
           <div class={ns.e('nav')} part={compParts[1]}>
             <div class={ns.e('wrapper')} part={compParts[3]}>
               {tabs.value.map((t) => (
-                <div>{t.label}</div>
+                <div class={[ns.e('tab'), ns.is('disabled', t.disabled)]} part={compParts[5]} onClick={t.onClick}>
+                  {t.label}
+                </div>
               ))}
             </div>
           </div>
           <TransitionGroup {...getTransitionProps(props, 'panel', 'panel')} {...transitionAttrs}>
-            {tabs.value.map((t, i) => {
-              const active = key === t.key || (key === undefined && i === 0);
-              return active ||
-                (t.forceRender ?? forceRender) ||
-                (showedKeys.has(t.key) && !(t.destroyInactive ?? destroyInactive))
-                ? withDirectives(
-                    <div key={t.key ?? i} class={ns.e('panel')} part={compParts[4]}>
+            {tabs.value
+              .map((t, i) => {
+                const active = localActive.value === t.slot;
+                return (
+                  (active ||
+                    (t.forceRender ?? forceRender) ||
+                    (showedKeys.has(t.slot) && !(t.destroyInactive ?? destroyInactive))) && (
+                    <div key={t.slot ?? i} class={ns.e('panel')} part={compParts[4]} v-show={active}>
                       {renderCustom(t.content)}
-                    </div>,
-                    [[vShow, active]],
+                    </div>
                   )
-                : undefined;
-            })}
+                );
+              })
+              // must filter, TransitionGroup requires valid child with key
+              .filter(Boolean)}
           </TransitionGroup>
         </div>
       );
--- packages/components/src/components/tabs/type.ts
+++ packages/components/src/components/tabs/type.ts
@@ -1,26 +1,22 @@
 import { freeze } from '@lun/utils';
-import {
-  GetEventPropsFromEmits,
-  PropBoolean,
-  themeProps,
-  createTransitionProps,
-  CommonProps,
-  PropArray,
-} from 'common';
+import { GetEventPropsFromEmits, PropBoolean, themeProps, createTransitionProps, CommonProps, PropArray, PropString } from 'common';
 import { ExtractPropTypes } from 'vue';
 
 export type TabItem = {
-  key?: string;
+  slot?: string;
   label: string;
   closable?: boolean;
   content: any;
   forceRender?: boolean;
   destroyInactive?: boolean;
+  disabled?: boolean;
 };
 
 export const tabsProps = freeze({
   ...themeProps,
   ...createTransitionProps('panel'),
+  activeSlot: PropString(),
+  defaultActiveSlot: PropString(),
   items: PropArray<TabItem[]>(),
   closable: PropBoolean(),
   forceRender: PropBoolean(),
@@ -28,6 +24,7 @@ export const tabsProps = freeze({
 });
 
 export const tabsEmits = freeze({
+  update: (_: string) => null,
   remove: null,
   afterRemove: null,
 });
@@ -35,3 +32,11 @@ export const tabsEmits = freeze({
 export type TabsSetupProps = ExtractPropTypes<typeof tabsProps> & CommonProps;
 export type TabsEvents = GetEventPropsFromEmits<typeof tabsEmits>;
 export type TabsProps = Partial<TabsSetupProps> & TabsEvents;
+
+export const tabItemProps = freeze({});
+
+export const tabItemEmits = freeze({});
+
+export type TabItemSetupProps = ExtractPropTypes<typeof tabItemProps> & CommonProps;
+export type TabItemEvents = GetEventPropsFromEmits<typeof tabItemEmits>;
+export type TabItemProps = Partial<TabItemSetupProps> & TabItemEvents;
