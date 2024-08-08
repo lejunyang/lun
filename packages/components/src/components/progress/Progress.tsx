b3711be57f5cbb6b32ae65d76936b70edc1c654b
lejunyang <lejunyang@qq.com>
2024-08-08 20:43:37 +0800

    feat(components/progress): add type page-top, add trailerStyle and strokeStyle

--- packages/components/src/components/progress/Progress.tsx
+++ packages/components/src/components/progress/Progress.tsx
@@ -6,77 +6,92 @@ import { computed } from 'vue';
 import { getCompParts, renderStatusIcon } from 'common';
 import { defineIcon } from '../icon/Icon';
 import { toPxIfNum } from '@lun/utils';
+import { methods } from './progress.static-methods';
 
 const name = 'progress';
-const parts = ['root', 'text'] as const;
+const parts = ['root', 'text', 'stroke'] as const;
 const compParts = getCompParts(name, parts);
-export const Progress = defineSSRCustomElement({
-  name,
-  props: progressProps,
-  setup(props) {
-    const ns = useNamespace(name);
+export const Progress = Object.assign(
+  defineSSRCustomElement({
+    name,
+    props: progressProps,
+    setup(props, { emit }) {
+      const ns = useNamespace(name);
+      const isPageTop = () => props.type === 'page-top',
+        isLine = () => isPageTop() || props.type === 'line',
+        isWave = () => props.type === 'wave';
 
-    const getInner = () => {
-      const { type, strokeColor } = props;
-      if (type === 'wave' || type === 'line') {
-        if (type === 'wave' && !numValue.value) return;
-        return (
-          <div
-            class={ns.e(type)}
-            style={{
-              backgroundColor: strokeColor,
-            }}
-          ></div>
+      const transitionEnd = () => {
+        if (numValue.value === 100) emit('done');
+      };
+
+      const getStroke = () => {
+        const { type, strokeColor, strokeStyle } = props;
+        if (isWave() || isLine()) {
+          if (isWave() && !numValue.value) return;
+          return (
+            <div
+              class={ns.e(type)}
+              part={compParts[2]}
+              onTransitionend={transitionEnd}
+              style={{
+                backgroundColor: strokeColor,
+                ...strokeStyle,
+              }}
+            ></div>
+          );
+        }
+      };
+
+      const numValue = computed(() => +props.value! || 0);
+      const percent = computed(() => numValue.value + '%');
+
+      const rootStyles = computed(() => {
+        let { trailerColor, width, height, trailerStyle } = props;
+        width = toPxIfNum(width);
+        height = toPxIfNum(height);
+        if (isWave() || isLine()) {
+          return {
+            backgroundColor: trailerColor,
+            ...trailerStyle,
+            ...ns.v({
+              value: percent.value,
+              'num-value': numValue.value / 100,
+              width,
+              height,
+            }),
+          };
+        }
+      });
+
+      return () => {
+        const { noPercent, status, showStatusIcon, type } = props;
+        const text = (
+          <span class={[ns.em('text', type), ns.e('text')]} part={compParts[1]}>
+            <slot>
+              {(showStatusIcon &&
+                renderStatusIcon(status, {
+                  noCircle: isWave(),
+                })) ||
+                (!noPercent && !isPageTop() && percent.value)}
+            </slot>
+          </span>
         );
-      }
-    };
-
-    const numValue = computed(() => +props.value! || 0);
-    const percent = computed(() => numValue.value + '%');
-
-    const rootStyles = computed(() => {
-      let { type, trailerColor, width, height } = props;
-      width = toPxIfNum(width);
-      height = toPxIfNum(height);
-      if (type === 'wave' || type === 'line') {
-        return {
-          backgroundColor: trailerColor,
-          ...ns.v({
-            value: percent.value,
-            'num-value': numValue.value / 100,
-            width,
-            height,
-          }),
-        };
-      }
-    });
-
-    return () => {
-      const { noPercent, status, showStatusIcon, type } = props;
-      const text = (
-        <span class={[ns.em('text', type), ns.e('text')]} part={compParts[1]}>
-          <slot>
-            {(showStatusIcon &&
-              renderStatusIcon(status, {
-                noCircle: type === 'wave',
-              })) ||
-              (!noPercent && percent.value)}
-          </slot>
-        </span>
-      );
-      const isLine = type === 'line';
-      return (
-        <>
-          <div class={[ns.t, ns.m(type)]} part={compParts[0]} style={rootStyles.value}>
-            {getInner()}
-            {!isLine && text}
-          </div>
-          {isLine && text}
-        </>
-      );
-    };
-  },
-});
+        const line = isLine();
+        return (
+          <>
+            <div class={[ns.t, ns.m(type)]} part={compParts[0]} style={rootStyles.value}>
+              {getStroke()}
+              {!line && text}
+            </div>
+            {line && text}
+          </>
+        );
+      };
+    },
+  }),
+  methods,
+);
 
 export type tProgress = typeof Progress;
 export type iProgress = InstanceType<typeof Progress>;
--- packages/components/src/components/progress/type.ts
+++ packages/components/src/components/progress/type.ts
@@ -1,11 +1,20 @@
 import { freeze } from '@lun/utils';
-import { CommonProps, GetEventPropsFromEmits, PropBoolean, PropNumber, PropString, Status, themeProps } from 'common';
-import { ExtractPropTypes } from 'vue';
+import {
+  CommonProps,
+  GetEventPropsFromEmits,
+  PropBoolean,
+  PropNumber,
+  PropObject,
+  PropString,
+  Status,
+  themeProps,
+} from 'common';
+import { CSSProperties, ExtractPropTypes } from 'vue';
 
 export const progressProps = freeze({
   ...themeProps,
   value: PropNumber(),
-  type: PropString<'wave' | 'ring' | 'line' | 'steps'>(),
+  type: PropString<'wave' | 'ring' | 'line' | 'steps' | 'page-top'>(),
   noPercent: PropBoolean(),
   status: PropString<Status>(),
   showStatusIcon: PropBoolean(),
@@ -13,9 +22,13 @@ export const progressProps = freeze({
   trailerColor: PropString(),
   width: PropNumber(),
   height: PropNumber(),
+  trailerStyle: PropObject<CSSProperties>(),
+  strokeStyle: PropObject<CSSProperties>(),
 });
 
-export const progressEmits = freeze({});
+export const progressEmits = freeze({
+  done: null,
+});
 
 export type ProgressSetupProps = ExtractPropTypes<typeof progressProps> & CommonProps;
 export type ProgressEvents = GetEventPropsFromEmits<typeof progressEmits>;
