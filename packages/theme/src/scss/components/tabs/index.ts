58421317ef358bc5350b67fcf1d46813debf8537
lejunyang <lejunyang@qq.com>
2024-08-09 19:35:00 +0800

    feat(theme/tabs): solid variant style

--- packages/theme/src/scss/components/tabs/index.ts
+++ packages/theme/src/scss/components/tabs/index.ts
@@ -1,4 +1,6 @@
 import { createImportStyle } from '@lun/components';
 import basic from './basic.scss?inline';
+import solid from './solid.scss?inline';
 
 export const importTabsBasicTheme = createImportStyle('tabs', basic);
+export const importTabsSolidTheme = createImportStyle('tabs', solid, 'solid');
