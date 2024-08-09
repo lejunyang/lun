e1ec3d3ebec4fe4180b4846fa4e69637c3e00ec3
lejunyang <lejunyang@qq.com>
2024-08-09 10:17:24 +0800

    feat(components/progress): support reuse, return done promise for stop

--- packages/components/src/components/progress/progress.static-methods.ts
+++ packages/components/src/components/progress/progress.static-methods.ts
@@ -1,4 +1,4 @@
-import { on, raf, runIfFn } from '@lun/utils';
+import { on, raf, runIfFn, withResolvers } from '@lun/utils';
 import { createLunElement, getFirstThemeProvider, toElement } from 'utils';
 import { iProgress } from './Progress';
 
@@ -7,10 +7,12 @@ export const methods = {
     getContainer,
     interval = 200,
     zIndex = 1000,
+    destroyOnDone = true,
   }: {
     getContainer?: () => Element | string;
     interval?: number;
     zIndex?: number;
+    destroyOnDone?: boolean;
   } = {}) {
     let container: Element | null = null;
     container = toElement(runIfFn(getContainer)) || getFirstThemeProvider() || document.body;
@@ -18,16 +20,22 @@ export const methods = {
     let value = 0;
     Object.assign(progress, {
       type: 'page-top',
-      value,
       trailerStyle: {
         zIndex,
       },
-      strokeStyle: {
-        transition: '0.2s linear',
-      },
     });
-    // TODO destroyOnDone, opacity: 0
-    on(progress, 'done', () => progress.remove());
+    const hide = () => (progress.style.cssText = `opacity: 0;transition: 0.1s;pointer-events: none;`);
+    hide();
+    const init = () => {
+      value = 0;
+      progress.style.cssText = ``;
+      Object.assign(progress, {
+        value,
+        strokeStyle: {
+          transition: '0.2s linear',
+        },
+      });
+    };
     const inc = () => {
       if (value >= 0 && value < 20) value += 1;
       else if (value >= 20 && value < 50) value += 0.4;
@@ -36,19 +44,35 @@ export const methods = {
       progress.value = value;
     };
 
+    let id: any, stopResolve: any;
+    on(progress, 'done', () => {
+      stopResolve?.();
+      stopResolve = null;
+      if (destroyOnDone) progress.remove();
+      else hide();
+    });
     container.append(progress);
-    let id: any;
+    /** returns a promise that resolves when the progress is done */
+    const stop = () => {
+      clearInterval(id);
+      progress.strokeStyle = { transition: '0.2s' }; // change to ease and then stop
+      progress.value = 100;
+      const { promise, resolve } = withResolvers();
+      stopResolve = resolve;
+      return promise;
+    };
     return {
       start(instant = true) {
+        if (!progress.isConnected) return;
         clearInterval(id);
-        progress.value = 0;
+        init();
         instant && raf(inc);
         id = setInterval(inc, interval);
       },
-      stop() {
-        clearInterval(id);
-        progress.strokeStyle = { transition: '0.2s' }; // change to ease and then stop
-        progress.value = 100;
+      stop,
+      destroy() {
+        stop();
+        progress.remove();
       },
     };
   },
