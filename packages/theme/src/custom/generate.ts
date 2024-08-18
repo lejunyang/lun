9e5865708f2a5360fd8ee4ccdbdaf4d27b3e2bd5
lejunyang <lejunyang@qq.com>
2024-08-18 23:18:57 +0800

    feat(theme/custom): add other dynamic accent color for the theme

--- packages/theme/src/custom/generate.ts
+++ packages/theme/src/custom/generate.ts
@@ -1,6 +1,6 @@
 import {
   createImportDynamicStyle,
-  getThemeValueFromInstance,
+  getThemeValue,
   GlobalStaticConfig,
   TGlobalContextConfig,
   allColorSet,
@@ -11,22 +11,28 @@ export const importCustomDynamicColors = createImportDynamicStyle('common', (vm,
   const { namespace } = context as TGlobalContextConfig;
   const n = namespace || GlobalStaticConfig.namespace;
   const { commonSeparator } = GlobalStaticConfig;
-  const getTheme = (prop: string) => getThemeValueFromInstance(vm, prop, context, name);
+  const getTheme = (prop: string) => getThemeValue(vm, prop, context, name);
   const color = getTheme('color');
-  function getVarName(...list: (string | number)[]) {
-    return '--' + n + commonSeparator + list.join(commonSeparator);
+  function getVar(list: (string | number)[], value: string) {
+    return '--' + n + commonSeparator + list.join(commonSeparator) + ':' + value + ';';
   }
   if (color && !allColorSet.has(color)) {
     const appearance = getTheme('appearance');
-    const { accentScale, accentScaleAlpha } = generateRadixColors({
+    const { accentScale, accentScaleAlpha, accentContrast, accentSurface } = generateRadixColors({
       appearance,
       accent: color,
       background: appearance === 'dark' ? '#111111' : '#FFFFFF',
       gray: '#8B8D98',
     });
-    return `:host{${accentScale.map((c, i) => `${getVarName('accent', i + 1)}:${c};`).join('')}${accentScaleAlpha
-      .map((c, i) => `${getVarName('accent', 'a' + (i + 1))}:${c};`)
-      .join('')}}`;
+    return `:host{${accentScale.map((c, i) => `${getVar(['accent', i + 1], c)}`).join('')}${accentScaleAlpha
+      .map((c, i) => `${getVar(['accent', 'a' + (i + 1)], c)}`)
+      .join('')}${getVar(['color', 'surface', 'accent'], accentSurface)}${getVar(
+      ['accent', '9', 'contrast'],
+      accentContrast,
+    )}${getVar(['color', 'focus'], accentScale[7])}${getVar(['color', 'selection'], accentScaleAlpha[4])}${getVar(
+      ['color', 'autofill'],
+      accentScaleAlpha[2],
+    )}}`;
   }
   return '';
 });
