9837fe381e23d88154642eaa8f9b24fdb818b493
lejunyang <lejunyang@qq.com>
2024-08-22 23:39:10 +0800

    feat(theme/custom): add grayColor, fix color parse error caused by keyword color

--- packages/theme/src/custom/generate.ts
+++ packages/theme/src/custom/generate.ts
@@ -5,6 +5,7 @@ import {
   TGlobalContextConfig,
   allColorSet,
 } from '@lun/components';
+import * as RadixColors from '@radix-ui/colors';
 import { generateRadixColors } from './custom';
 
 export const importCustomDynamicColors = createImportDynamicStyle('common', (vm, name, context) => {
@@ -12,17 +13,24 @@ export const importCustomDynamicColors = createImportDynamicStyle('common', (vm,
   const n = namespace || GlobalStaticConfig.namespace;
   const { commonSeparator } = GlobalStaticConfig;
   const getTheme = (prop: string) => getThemeValue(vm, prop, context, name);
-  const color = getTheme('color');
+  const color = getTheme('color'),
+    grayColor = getTheme('grayColor');
+  const appearance = getTheme('appearance'),
+    isDark = appearance === 'dark';
   function getVar(list: (string | number)[], value: string) {
     return '--' + n + commonSeparator + list.join(commonSeparator) + ':' + value + ';';
   }
-  if (color && !allColorSet.has(color)) {
-    const appearance = getTheme('appearance');
+
+  const getKeywordColor = (name: string) => {
+    // @ts-ignore
+    return allColorSet.has(name) && RadixColors[name + (isDark ? 'Dark' : '')][name + '9'];
+  };
+  if ((color && !allColorSet.has(color)) || (grayColor && !allColorSet.has(grayColor))) {
     const { accentScale, accentScaleAlpha, accentContrast, accentSurface } = generateRadixColors({
       appearance,
-      accent: color,
-      background: appearance === 'dark' ? '#111111' : '#FFFFFF',
-      gray: '#8B8D98',
+      accent: getKeywordColor(color) || color, // some keywords are not standard css colors(like slate, bronze)
+      background: isDark ? '#111111' : '#FFFFFF',
+      gray: getKeywordColor(grayColor) || grayColor,
     });
     return `:host{${accentScale.map((c, i) => `${getVar(['accent', i + 1], c)}`).join('')}${accentScaleAlpha
       .map((c, i) => `${getVar(['accent', 'a' + (i + 1)], c)}`)
