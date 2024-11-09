0942aea1f44be58cdc63e6d5c398deb72aead70c
lejunyang <lejunyang@qq.com>
2024-11-09 22:02:37 +0800

    feat(core): add `useSet`, `useMap`, `useWeakSet`, `useWeakMap`

--- .changeset/core.md
+++ .changeset/core.md
@@ -2,7 +2,11 @@
 '@lun-web/core': patch
 ---
 
+### Features
 
-## Bug Fixes
+- add `createUnrefBinds`
+- add `useSet`, `useMap`, `useWeakSet`, `useWeakMap` hooks
+
+### Bug Fixes
 
 - `core/createCollector`: parent element can be undefined when adding item; invoke getParentEl if collectOnSetup is true
--- packages/core/src/hooks/state.ts
+++ packages/core/src/hooks/state.ts
@@ -1,6 +1,6 @@
-import { isFunction, runIfFn, ensureArray } from '@lun-web/utils';
+import { isFunction, runIfFn, ensureArray, globalObject } from '@lun-web/utils';
 import { computed, ref, shallowRef, watchEffect, Ref, watch, WatchOptions } from 'vue';
-import { MaybeRefLikeOrGetter, unrefOrGet } from '../utils/ref';
+import { createUnrefBinds, MaybeRefLikeOrGetter, unrefOrGet } from '../utils/ref';
 
 /**
  * create a temporary ref，which means ref value is initialized with getter and you can change it as you want, but it will reset the value when getter updates
@@ -108,3 +108,72 @@ export function cacheComputed<T>(getter: (oldVal?: NoInfer<T>) => T, shouldUpdat
     else return cache;
   });
 }
+
+const createUseSetOrMap =
+  <TStr extends 'Set' | 'Map' | 'WeakSet' | 'WeakMap', T = InstanceType<(typeof globalThis)[TStr]>>(type: TStr) =>
+  () => {
+    const constructor = globalObject[type] as any,
+      result = ref<T>(new constructor()),
+      isSet = type === 'Set' || type === 'WeakSet';
+    // @ts-expect-error
+    const binds = createUnrefBinds(result, 'add', 'has', 'set', 'get', 'delete', 'forEach') as any,
+      replace = (newObj: T = new constructor()) => (result.value = newObj);
+    return Object.assign(result, {
+      replace,
+      [Symbol.iterator]: function* () {
+        yield replace;
+        yield isSet ? binds.has : binds.get;
+        yield isSet ? binds.add : binds.set;
+        yield binds.delete;
+      },
+      ...binds,
+    });
+  };
+
+type SetOrMap = Set<any> | WeakSet<any> | Map<any, any> | WeakMap<any, any>;
+type GetIterateMethods<T extends SetOrMap, Methods> = T extends Set<any> | WeakSet<any>
+  ? // @ts-expect-error
+    [replace: Methods['replace'], has: Methods['has'], add: Methods['add'], del: Methods['delete']]
+  : T extends Map<any, any> | WeakMap<any, any>
+  ? // @ts-expect-error
+    [replace: Methods['replace'], get: Methods['get'], set: Methods['set'], del: Methods['delete']]
+  : never;
+
+type GetIterableRefWithMethods<
+  T extends SetOrMap,
+  M extends readonly string[],
+  K extends keyof T = M[number] extends keyof T ? M[number] : never,
+  Methods = Pick<T, K> & { replace: (newObj?: T) => T },
+> = Ref<T> & Methods & GetIterateMethods<T, Methods>;
+
+/**
+ * create a Set ref with add, has, delete, forEach and replace(replace ref's value with a new Set) methods, the ref is also an iterable object containing [replace, has, add, delete] methods
+ */
+export const useSet = createUseSetOrMap('Set') as any as <K>() => GetIterableRefWithMethods<
+  Set<K>,
+  ['add', 'has', 'delete', 'forEach']
+>;
+
+/**
+ * create a WeakSet ref with add, has, delete and replace(replace ref's value with a new WeakSet) methods, the ref is also an iterable object containing [replace, has, add, delete] methods
+ */
+export const useWeakSet = createUseSetOrMap('WeakSet') as any as <K extends WeakKey>() => GetIterableRefWithMethods<
+  WeakSet<K>,
+  ['add', 'has', 'delete']
+>;
+
+/**
+ * create a Map ref with get, has, set, delete, forEach and replace(replace ref's value with a new Map) methods, the ref is also an iterable object containing [replace, get, set, delete] methods
+ */
+export const useMap = createUseSetOrMap('Map') as any as <K, V>() => GetIterableRefWithMethods<
+  Map<K, V>,
+  ['get', 'has', 'set', 'delete', 'forEach']
+>;
+
+/**
+ * create a WeakMap ref with get, has, set, delete and replace(replace ref's value with a new WeakMap) methods, the ref is also an iterable object containing [replace, get, set, delete] methods
+ */
+export const useWeakMap = createUseSetOrMap('WeakMap') as any as <K extends WeakKey, V>() => GetIterableRefWithMethods<
+  WeakMap<K, V>,
+  ['get', 'has', 'set', 'delete']
+>;
